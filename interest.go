5b2f9b7807835c385cd589397771f2428e86c3a3
noib <b_n_79@protonmail.com>
2024-08-15 23:16:27 +0800

    Change RecommendationsById  type to pq.Int32Array

--- interest.go
+++ interest.go
@@ -14,6 +14,6 @@ type Interest struct {
 	DeletedAt             *time.Time     `gorm:"index" json:"-,omitempty"`
 	Description           string         `gorm:"type:text" json:"description"`
 	RecommendationsByNpub pq.StringArray `gorm:"type:varchar[]" json:"recommendations_by_npub"`
-	RecommendationsById   []uint         `gorm:"type:integer[]" json:"recommendations_by_id"`
+	RecommendationsById   pq.Int32Array  `gorm:"type:integer[]" json:"recommendations_by_id"`
 	Hashtags              pq.StringArray `gorm:"type:varchar[]" json:"hashtags"`
 }
