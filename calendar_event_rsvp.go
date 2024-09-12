0b53a3cc8b83dca5b060981210078d2c0bfbc390
SatoshiSound <satoshisound@protonmail.com>
2024-09-12 18:55:43 +0100

    updates to calendar_event_rsvp

--- calendar_event_rsvp.go
+++ calendar_event_rsvp.go
@@ -1,10 +1,20 @@
 package models
 
+import "time"
+
 type CalendarEventRSVP struct {
-	ID        uint    `gorm:"primaryKey" json:"id"`
-	AccountID uint    `gorm:"index;not null" json:"accountId"`
-	Account   Account `json:"account"`
-	EventID   uint    `gorm:"index;not null" json:"eventId"`
-	NoteID    uint    `gorm:"index;not null" json:"noteId"`
-	Status    string  `json:"status"`
+	ID              uint      `gorm:"primaryKey" json:"id"`
+	AccountID       uint      `gorm:"index;not null" json:"accountId"`
+	Account         Account   `json:"account"`
+	CreatedAt       time.Time `json:"createdAt"`
+	Content         string    `gorm:"type:text" json:"content"`
+	EventID         uint      `gorm:"index" json:"eventId"`
+	Event           Event     `json:"event"`
+	NostrID         string    `gorm:"index" json:"nostrId"`
+	PubKey          string    `gorm:"type:text;index" json:"pubkey"`
+	Sig             string    `gorm:"type:text" json:"sig"`
+	Tags            string    `gorm:"type:jsonb" json:"tags"`
+	CalendarEventID uint      `gorm:"index;not null" json:"calendarEventId"`
+	NoteID          uint      `gorm:"index;not null" json:"noteId"` // Deprecated
+	Status          string    `json:"status"`
 }
