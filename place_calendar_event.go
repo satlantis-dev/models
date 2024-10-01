55a50b4e5c6cb2350a5db0b6ef5210950661ce6e
SatoshiSound <satoshisound@protonmail.com>
2024-10-01 14:03:01 +0100

    return createdAt for PlaceCalendarEvent

--- place_calendar_event.go
+++ place_calendar_event.go
@@ -4,7 +4,7 @@ import "time"
 
 type PlaceCalendarEvent struct {
 	ID              uint          `gorm:"primaryKey" json:"id"`
-	CreatedAt       time.Time     `json:"-"`
+	CreatedAt       time.Time     `json:"createdAt"`
 	UpdatedAt       time.Time     `json:"-"`
 	DeletedAt       *time.Time    `gorm:"index" json:"-,omitempty"`
 	PlaceID         uint          `gorm:"index" json:"placeId"`
