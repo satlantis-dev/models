16af8fb8ed9b7c0daddf451b7f03e01796d41708
SatoshiSound <satoshisound@protonmail.com>
2024-10-03 13:14:49 +0100

    remove calendarEventRsvps from notes

--- note.go
+++ note.go
@@ -22,24 +22,23 @@ const (
 )
 
 type Note struct {
-	ID                 uint                `gorm:"primaryKey" json:"id"`
-	AccountID          uint                `gorm:"index" json:"accountId"`
-	Account            Account             `json:"account"`
-	CreatedAt          *time.Time          `json:"createdAt"`
-	CalendarEventRSVPs []CalendarEventRSVP `gorm:"foreignKey:NoteID" json:"calendarEventRsvps"`
-	ChatMemberships    []ChatMembership    `gorm:"foreignKey:NoteID" json:"chatMemberships"`
-	Content            *string             `gorm:"type:text" json:"content"`
-	Descendants        []NoteWithClosure   `gorm:"-" json:"descendants"`
-	EventID            uint                `gorm:"index" json:"eventId"`
-	Kind               uint                `gorm:"index" json:"kind"`
-	NostrID            string              `gorm:"index" json:"nostrId"`
-	PubKey             string              `gorm:"type:text;index" json:"pubkey"`
-	Sig                string              `gorm:"type:text" json:"sig"`
-	Tags               *string             `gorm:"type:jsonb" json:"tags"`
-	Type               NoteType            `json:"type"`
-	RepostedNoteID     *uint               `gorm:"index" json:"repostedNoteId"`
-	RepostedNote       *Note               `json:"reposted_note"`
-	Reactions          []Reaction          `gorm:"foreignKey:NoteID" json:"reactions"`
+	ID              uint              `gorm:"primaryKey" json:"id"`
+	AccountID       uint              `gorm:"index" json:"accountId"`
+	Account         Account           `json:"account"`
+	CreatedAt       *time.Time        `json:"createdAt"`
+	ChatMemberships []ChatMembership  `gorm:"foreignKey:NoteID" json:"chatMemberships"`
+	Content         *string           `gorm:"type:text" json:"content"`
+	Descendants     []NoteWithClosure `gorm:"-" json:"descendants"`
+	EventID         uint              `gorm:"index" json:"eventId"`
+	Kind            uint              `gorm:"index" json:"kind"`
+	NostrID         string            `gorm:"index" json:"nostrId"`
+	PubKey          string            `gorm:"type:text;index" json:"pubkey"`
+	Sig             string            `gorm:"type:text" json:"sig"`
+	Tags            *string           `gorm:"type:jsonb" json:"tags"`
+	Type            NoteType          `json:"type"`
+	RepostedNoteID  *uint             `gorm:"index" json:"repostedNoteId"`
+	RepostedNote    *Note             `json:"reposted_note"`
+	Reactions       []Reaction        `gorm:"foreignKey:NoteID" json:"reactions"`
 }
 
 type NoteWithClosure struct {
