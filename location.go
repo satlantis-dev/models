8a8ff3730be826ab46237e507fc3dd36a0d386af
SatoshiSound <satoshisound@protonmail.com>
2024-10-10 09:35:05 +0100

    remove location.accounts

--- location.go
+++ location.go
@@ -78,42 +78,40 @@ func (o OpeningHours) Value() (driver.Value, error) {
 }
 
 type Location struct {
-	ID                    uint              `gorm:"primaryKey" json:"id"`
-	CreatedAt             time.Time         `json:"-"`
-	UpdatedAt             time.Time         `json:"-"`
-	DeletedAt             *time.Time        `gorm:"index" json:"-,omitempty"`
-	Accounts              []LocationAccount `gorm:"foreignKey:LocationID" json:"accounts"`
-	Address               Address           `gorm:"type:jsonb" json:"address"`
-	Bio                   *string           `json:"bio"`
-	BusinessStatus        BusinessStatus    `gorm:"type:text" json:"businessStatus"`
-	EventID               *uint             `gorm:"index" json:"eventId"`
-	Event                 Event             `json:"event"`
-	GoogleID              string            `gorm:"uniqueIndex;not null" json:"googleId"`
-	GoogleMapsUrl         string            `json:"googleMapsUrl"`
-	GoogleRating          float64           `json:"googleRating"`
-	GoogleUserRatingCount int               `json:"googleUserRatingCount"`
-	Image                 string            `json:"image"`
-	IsClaimed             bool              `json:"isClaimed"`
-	Lat                   float64           `json:"lat"`
-	Lng                   float64           `json:"lng"`
-	LocationTags          []LocationTag     `gorm:"many2many:location_location_tags" json:"locationTags"`
-	PlaceID               uint              `gorm:"index" json:"placeId"`
-	Place                 Place             `json:"place"`
-	Name                  string            `json:"name"`
-	Notes                 []LocationNote    `gorm:"foreignKey:LocationID" json:"notes"`
-	OpeningHours          OpeningHours      `gorm:"type:jsonb" json:"openingHours"`
-	OSMRef                string            `gorm:"uniqueIndex;not null" json:"osmRef"`
-	Phone                 string            `json:"phone"`
-	PriceLevel            PriceLevel        `json:"priceLevel"`
-	Score                 float64           `json:"score"`
-	WebsiteUrl            string            `json:"websiteUrl"`
-	Email                 string            `json:"email"`
+	ID                    uint           `gorm:"primaryKey" json:"id"`
+	CreatedAt             time.Time      `json:"-"`
+	UpdatedAt             time.Time      `json:"-"`
+	DeletedAt             *time.Time     `gorm:"index" json:"-,omitempty"`
+	Address               Address        `gorm:"type:jsonb" json:"address"`
+	Bio                   *string        `json:"bio"`
+	BusinessStatus        BusinessStatus `gorm:"type:text" json:"businessStatus"`
+	EventID               *uint          `gorm:"index" json:"eventId"`
+	Event                 Event          `json:"event"`
+	GoogleID              string         `gorm:"uniqueIndex;not null" json:"googleId"`
+	GoogleMapsUrl         string         `json:"googleMapsUrl"`
+	GoogleRating          float64        `json:"googleRating"`
+	GoogleUserRatingCount int            `json:"googleUserRatingCount"`
+	Image                 string         `json:"image"`
+	IsClaimed             bool           `json:"isClaimed"`
+	Lat                   float64        `json:"lat"`
+	Lng                   float64        `json:"lng"`
+	LocationTags          []LocationTag  `gorm:"many2many:location_location_tags" json:"locationTags"`
+	PlaceID               uint           `gorm:"index" json:"placeId"`
+	Place                 Place          `json:"place"`
+	Name                  string         `json:"name"`
+	Notes                 []LocationNote `gorm:"foreignKey:LocationID" json:"notes"`
+	OpeningHours          OpeningHours   `gorm:"type:jsonb" json:"openingHours"`
+	OSMRef                string         `gorm:"uniqueIndex;not null" json:"osmRef"`
+	Phone                 string         `json:"phone"`
+	PriceLevel            PriceLevel     `json:"priceLevel"`
+	Score                 float64        `json:"score"`
+	WebsiteUrl            string         `json:"websiteUrl"`
+	Email                 string         `json:"email"`
 }
 
 // LocationDTO
 type LocationDTO struct {
 	ID                    uint          `json:"id"`
-	Accounts              []AccountDTO  `json:"accounts"`
 	Bio                   *string       `json:"bio"`
 	Image                 string        `json:"image"`
 	Lat                   float64       `json:"lat"`
@@ -132,29 +130,12 @@ type LocationDTO struct {
 }
 
 func (l Location) ToDTO(db *gorm.DB) (*LocationDTO, error) {
-	var accounts []Account
-	// Get the accounts
-	err := db.Table("location_accounts").Select("accounts.*").
-		Joins("join accounts on accounts.id = location_accounts.account_id").
-		Where("location_accounts.location_id = ?", l.ID).
-		Scan(&accounts).Error
-	if err != nil {
-		return nil, err
-	}
-
-	accountDTOs := make([]AccountDTO, len(accounts))
-
-	for i, account := range accounts {
-		accountDTOs[i] = account.ToDTO()
-	}
-
 	// Get the place
 	var place Place
 	_ = db.First(&place, l.PlaceID).Select("OSMRef").Error
 
 	return &LocationDTO{
 		ID:                    l.ID,
-		Accounts:              accountDTOs,
 		Bio:                   l.Bio,
 		Image:                 l.Image,
 		Lat:                   l.Lat,
