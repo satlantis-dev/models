d4cca727206539989562c81ed0c39ca555a3f185
satoshisound <robertwilliamallen@gmail.com>
2024-11-04 09:21:26 +0100

    added Account back to Reaction

--- reaction.go
+++ reaction.go
@@ -1,9 +1,10 @@
 package models
 
 type Reaction struct {
-	ID        uint  `gorm:"primaryKey" json:"id"`
-	AccountID uint  `gorm:"index;not null" json:"accountId"`
-	EventID   uint  `gorm:"index;not null" json:"eventId"`
-	Event     Event `json:"event"`
-	NoteID    uint  `gorm:"index;not null" json:"noteId"`
+	ID        uint    `gorm:"primaryKey" json:"id"`
+	AccountID uint    `gorm:"index;not null" json:"accountId"`
+	Account   Account `json:"account"`
+	EventID   uint    `gorm:"index;not null" json:"eventId"`
+	Event     Event   `json:"event"`
+	NoteID    uint    `gorm:"index;not null" json:"noteId"`
 }
