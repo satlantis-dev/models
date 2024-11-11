1261d8ac5d5f29080b187e2845f865d8596d51b3
noib <b_n_79@protonmail.com>
2024-11-12 01:22:37 +0800

    Add tripadvisor fields

--- source_locations.go
+++ source_locations.go
@@ -12,24 +12,26 @@ const (
 
 // SourceLocationsOsm with Response as JSON object
 type SourceLocationsOsm struct {
-	OSMId          uint      `gorm:"primaryKey;type:uint" json:"osmId"`
-	OSMType        OSMType   `gorm:"primaryKey;type:string" json:"osmType"`
-	OSMRef         string    `gorm:"uniqueIndex" json:"osmRef"`
-	Name           string    `json:"name"`
-	Lat            float64   `json:"lat"`
-	Lng            float64   `json:"lng"`
-	OSMStdTags     string    `gorm:"type:jsonb" json:"osmStdTags"`
-	OSMExtraTags   string    `gorm:"type:jsonb" json:"osmExtraTags"`
-	SatlantisTags  string    `gorm:"type:jsonb" json:"satlantisTags"`
-	OSMDetails     string    `gorm:"type:jsonb" json:"osmDetails"`
-	OSMPlaceRef    string    `json:"osmPlaceRef"`
-	OSMPlaceName   string    `json:"osmPlaceName"`
-	Source         string    `gorm:"default:osm" json:"source"`
-	GoogleId       string    `json:"googleId"`
-	GoogleDetails  string    `gorm:"type:jsonb" json:"googleDetails"`
-	GooglePhotoUrl string    `json:"googlePhotoUrl"`
-	UpdatedOn      time.Time `json:"updatedOn"`
-	Eligible       bool      `json:"eligible"`
+	OSMId              uint      `gorm:"primaryKey;type:uint" json:"osmId"`
+	OSMType            OSMType   `gorm:"primaryKey;type:string" json:"osmType"`
+	OSMRef             string    `gorm:"uniqueIndex" json:"osmRef"`
+	Name               string    `json:"name"`
+	Lat                float64   `json:"lat"`
+	Lng                float64   `json:"lng"`
+	OSMStdTags         string    `gorm:"type:jsonb" json:"osmStdTags"`
+	OSMExtraTags       string    `gorm:"type:jsonb" json:"osmExtraTags"`
+	SatlantisTags      string    `gorm:"type:jsonb" json:"satlantisTags"`
+	OSMDetails         string    `gorm:"type:jsonb" json:"osmDetails"`
+	OSMPlaceRef        string    `json:"osmPlaceRef"`
+	OSMPlaceName       string    `json:"osmPlaceName"`
+	Source             string    `gorm:"default:osm" json:"source"`
+	GoogleId           string    `json:"googleId"`
+	GoogleDetails      string    `gorm:"type:jsonb" json:"googleDetails"`
+	GooglePhotoUrl     string    `json:"googlePhotoUrl"`
+	TripadvisorId      *uint     `json:"tripadvisorId"`
+	TripadvisorDetails string    `gorm:"type:jsonb" json:"tripadvisorDetails"`
+	UpdatedOn          time.Time `json:"updatedOn"`
+	Eligible           bool      `json:"eligible"`
 }
 
 func (SourceLocationsOsm) TableName() string {
@@ -38,24 +40,26 @@ func (SourceLocationsOsm) TableName() string {
 
 // SourceLocationsExtra with Response as JSON object
 type SourceLocationsExtra struct {
-	OSMId          uint      `gorm:"primaryKey;type:bigserial" json:"osmId"`
-	OSMType        OSMType   `gorm:"type:string;default:node" json:"osmType"`
-	OSMRef         string    `gorm:"uniqueIndex" json:"osmRef"`
-	Name           string    `json:"name"`
-	Lat            float64   `json:"lat"`
-	Lng            float64   `json:"lng"`
-	OSMStdTags     string    `gorm:"type:jsonb" json:"osmStdTags"`
-	OSMExtraTags   string    `gorm:"type:jsonb" json:"osmExtraTags"`
-	SatlantisTags  string    `gorm:"type:jsonb" json:"satlantisTags"`
-	OSMDetails     string    `gorm:"type:jsonb" json:"osmDetails"`
-	OSMPlaceRef    string    `json:"osmPlaceRef"`
-	OSMPlaceName   string    `json:"osmPlaceName"`
-	Source         string    `gorm:"not null" json:"source"`
-	GoogleId       string    `gorm:"uniqueIndex" json:"googleId"`
-	GoogleDetails  string    `gorm:"type:jsonb" json:"googleDetails"`
-	GooglePhotoUrl string    `json:"googlePhotoUrl"`
-	UpdatedOn      time.Time `json:"updatedOn"`
-	Eligible       bool      `json:"eligible"`
+	OSMId              uint      `gorm:"primaryKey;type:bigserial" json:"osmId"`
+	OSMType            OSMType   `gorm:"type:string;default:node" json:"osmType"`
+	OSMRef             string    `gorm:"uniqueIndex" json:"osmRef"`
+	Name               string    `json:"name"`
+	Lat                float64   `json:"lat"`
+	Lng                float64   `json:"lng"`
+	OSMStdTags         string    `gorm:"type:jsonb" json:"osmStdTags"`
+	OSMExtraTags       string    `gorm:"type:jsonb" json:"osmExtraTags"`
+	SatlantisTags      string    `gorm:"type:jsonb" json:"satlantisTags"`
+	OSMDetails         string    `gorm:"type:jsonb" json:"osmDetails"`
+	OSMPlaceRef        string    `json:"osmPlaceRef"`
+	OSMPlaceName       string    `json:"osmPlaceName"`
+	Source             string    `gorm:"not null" json:"source"`
+	GoogleId           string    `gorm:"uniqueIndex" json:"googleId"`
+	GoogleDetails      string    `gorm:"type:jsonb" json:"googleDetails"`
+	GooglePhotoUrl     string    `json:"googlePhotoUrl"`
+	TripadvisorId      *uint     `json:"tripadvisorId"`
+	TripadvisorDetails string    `gorm:"type:jsonb" json:"tripadvisorDetails"`
+	UpdatedOn          time.Time `json:"updatedOn"`
+	Eligible           bool      `json:"eligible"`
 }
 
 func (SourceLocationsExtra) TableName() string {
@@ -63,22 +67,24 @@ func (SourceLocationsExtra) TableName() string {
 }
 
 type SourceLocations struct {
-	OSMId          uint      `json:"osmId"`
-	OSMType        OSMType   `json:"osmType"`
-	OSMRef         string    `gorm:"uniqueIndex" json:"osmRef"`
-	Name           string    `json:"name"`
-	Lat            float64   `json:"lat"`
-	Lng            float64   `json:"lng"`
-	OSMStdTags     string    `json:"osmStdTags"`
-	OSMExtraTags   string    `json:"osmExtraTags"`
-	SatlantisTags  string    `json:"satlantisTags"`
-	OSMDetails     string    `json:"osmDetails"`
-	OSMPlaceRef    string    `json:"osmPlaceRef"`
-	OSMPlaceName   string    `json:"osmPlaceName"`
-	Source         string    `json:"source"`
-	GoogleId       string    `json:"googleId"`
-	GoogleDetails  string    `json:"googleDetails"`
-	GooglePhotoUrl string    `json:"googlePhotoUrl"`
-	UpdatedOn      time.Time `json:"updatedOn"`
-	Eligible       bool      `json:"eligible"`
+	OSMId              uint      `json:"osmId"`
+	OSMType            OSMType   `json:"osmType"`
+	OSMRef             string    `gorm:"uniqueIndex" json:"osmRef"`
+	Name               string    `json:"name"`
+	Lat                float64   `json:"lat"`
+	Lng                float64   `json:"lng"`
+	OSMStdTags         string    `json:"osmStdTags"`
+	OSMExtraTags       string    `json:"osmExtraTags"`
+	SatlantisTags      string    `json:"satlantisTags"`
+	OSMDetails         string    `json:"osmDetails"`
+	OSMPlaceRef        string    `json:"osmPlaceRef"`
+	OSMPlaceName       string    `json:"osmPlaceName"`
+	Source             string    `json:"source"`
+	GoogleId           string    `json:"googleId"`
+	GoogleDetails      string    `json:"googleDetails"`
+	GooglePhotoUrl     string    `json:"googlePhotoUrl"`
+	TripadvisorId      *uint     `json:"tripadvisorId"`
+	TripadvisorDetails string    `json:"tripadvisorDetails"`
+	UpdatedOn          time.Time `json:"updatedOn"`
+	Eligible           bool      `json:"eligible"`
 }
