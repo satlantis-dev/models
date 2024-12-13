782a9d88ca1c21188ac4a99082d2a33720e00390
satoshisound <robertwilliamallen@gmail.com>
2024-12-13 19:05:24 +0100

    add notification sent bools to cal event rsvp

--- calendar_event_rsvp.go
+++ calendar_event_rsvp.go
@@ -3,17 +3,20 @@ package models
 import "time"
 
 type CalendarEventRSVP struct {
-	ID              uint      `gorm:"primaryKey" json:"id"`
-	AccountID       uint      `json:"accountId"`
-	Account         Account   `json:"account"`
-	CreatedAt       time.Time `json:"createdAt"`
-	Content         string    `gorm:"type:text" json:"content"`
-	EventID         uint      `gorm:"index" json:"eventId"`
-	Kind            uint      `gorm:"index" json:"kind"`
-	NostrID         string    `gorm:"index" json:"nostrId"`
-	PubKey          string    `gorm:"type:text;index" json:"pubkey"`
-	Sig             string    `gorm:"type:text" json:"sig"`
-	Tags            string    `gorm:"type:jsonb" json:"tags"`
-	CalendarEventID uint      `json:"calendarEventId"`
-	Status          string    `json:"status"`
+	ID                   uint      `gorm:"primaryKey" json:"id"`
+	AccountID            uint      `json:"accountId"`
+	Account              Account   `json:"account"`
+	CreatedAt            time.Time `json:"createdAt"`
+	Content              string    `gorm:"type:text" json:"content"`
+	EventID              uint      `gorm:"index" json:"eventId"`
+	Kind                 uint      `gorm:"index" json:"kind"`
+	NostrID              string    `gorm:"index" json:"nostrId"`
+	PubKey               string    `gorm:"type:text;index" json:"pubkey"`
+	Sig                  string    `gorm:"type:text" json:"sig"`
+	Tags                 string    `gorm:"type:jsonb" json:"tags"`
+	CalendarEventID      uint      `json:"calendarEventId"`
+	Status               string    `json:"status"`
+	NotificationWeekSent bool      `gorm:"default:false"`
+	NotificationDaySent  bool      `gorm:"default:false"`
+	NotificationHourSent bool      `gorm:"default:false"`
 }
