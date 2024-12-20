bb6d8eb0890203f149fcd49cbc8dacc99eee8ec5
satoshisound <robertwilliamallen@gmail.com>
2024-12-20 12:11:05 +0100

    added imageUrl to Notification

--- notification.go
+++ notification.go
@@ -11,6 +11,7 @@ type Notification struct {
 	RecipientAccountID uint       `gorm:"index" json:"-"` // Account that will be notified
 	Type               string     `gorm:"type:varchar(255)" json:"type"`
 	Action             string     `gorm:"type:text" json:"action"`
+	ImageURL           string     `gorm:"type:text" json:"imageUrl"`
 	Link               string     `gorm:"type:text" json:"link"`
 	Message            string     `gorm:"type:text" json:"message"`
 	IsRead             bool       `gorm:"default:false" json:"isRead"`
