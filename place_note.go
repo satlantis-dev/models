4d9ab7018e338a3b46104485bbaad1365f810907
noib <b_n_79@protonmail.com>
2024-12-28 14:51:06 +0800

    Remove PlaceFeedNote and add Score to PlaceNote

--- place_note.go
+++ place_note.go
@@ -5,23 +5,17 @@ import (
 )
 
 type PlaceNote struct {
-	ID        uint       `gorm:"primaryKey" json:"id"`
-	CreatedAt time.Time  `json:"-"`
-	UpdatedAt time.Time  `json:"-"`
-	DeletedAt *time.Time `gorm:"index" json:"-,omitempty"`
-	PlaceID   uint       `gorm:"index" json:"placeId"`
-	NoteID    uint       `gorm:"index" json:"noteId"`
-	Note      Note       `json:"note"`
-	Nsfw      bool       `json:"nsfw"`
-	Type      NoteType   `json:"type"`
-}
-
-type PlaceFeedNote struct {
-	ID      uint     `json:"id"`
-	PlaceID uint     `json:"placeId"`
-	NoteID  uint     `json:"noteId"`
-	Note    FeedNote `json:"note"`
-	Type    NoteType `json:"type"`
+	ID          uint       `gorm:"primaryKey" json:"id"`
+	CreatedAt   time.Time  `json:"-"`
+	UpdatedAt   time.Time  `json:"-"`
+	DeletedAt   *time.Time `gorm:"index" json:"-,omitempty"`
+	PlaceID     uint       `gorm:"index" json:"placeId"`
+	NoteID      uint       `gorm:"index" json:"noteId"`
+	Note        Note       `json:"note"`
+	Nsfw        bool       `json:"nsfw"`
+	Type        NoteType   `json:"type"`
+	OnSatlantis bool       `json:"onSatlantis"`
+	Score       float64    `json:"score"`
 }
 
 type PlaceNoteWithClosure struct {
