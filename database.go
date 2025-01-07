02e8a8f34b371dd0c661e3f8f61e228388678b68
noib <b_n_79@protonmail.com>
2025-01-07 23:49:08 +0800

    Create brands table

--- database.go
+++ database.go
@@ -14,6 +14,7 @@ var DatabaseModels = []interface{}{
 	AccountNoteTracker{},
 	AccountPlaceRole{},
 	AuthenticationDetail{},
+	Brand{},
 	CalendarEventAnnouncement{},
 	CalendarEventNote{},
 	CalendarEventRSVP{},
