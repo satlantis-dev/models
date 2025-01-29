d8349fa751e4d886e9dc6f4fa1d98ba11b83aaba
noib <b_n_79@protonmail.com>
2025-01-29 22:37:21 +0800

    Remove type from LocationAccount

--- location_account.go
+++ location_account.go
@@ -4,23 +4,13 @@ import (
 	"time"
 )
 
-type LocationAccountType string
-
-const (
-	LocationAccountTypeOwner   LocationAccountType = "owner"
-	LocationAccountTypeManager LocationAccountType = "manager"
-	LocationAccountTypeMember  LocationAccountType = "member"
-	LocationAccountTypeVisitor LocationAccountType = "visitor"
-)
-
 type LocationAccount struct {
-	ID              uint                `gorm:"primaryKey" json:"id"`
-	LocationID      uint                `gorm:"index" json:"locationId"`
-	Location        Location            `json:"location"`
-	AccountID       uint                `gorm:"index" json:"accountId"`
-	Account         AccountDTO          `json:"account"`
-	Type            LocationAccountType `gorm:"not null" json:"type"`
-	ClaimCode       string              `gorm:"type:text" json:"claimCode"`
-	ClaimVerifiedAt *time.Time          `json:"-"`
-	ReferredBy      *string             `gorm:"type:text" json:"referredBy"`
+	ID              uint       `gorm:"primaryKey" json:"id"`
+	LocationID      uint       `gorm:"index" json:"locationId"`
+	Location        Location   `json:"location"`
+	AccountID       uint       `gorm:"index" json:"accountId"`
+	Account         AccountDTO `json:"account"`
+	ClaimCode       string     `gorm:"type:text" json:"claimCode"`
+	ClaimVerifiedAt *time.Time `json:"-"`
+	ReferredBy      *string    `gorm:"type:text" json:"referredBy"`
 }
