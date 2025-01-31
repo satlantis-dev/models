71058ec466e83f42fa3106417c7db2e6534a42a7
noib <b_n_79@protonmail.com>
2025-02-01 00:03:12 +0800

    Fix AccoutRoles relation

--- location.go
+++ location.go
@@ -103,41 +103,42 @@ func (r ExternalRating) Value() (driver.Value, error) {
 }
 
 type Location struct {
-	ID                    uint           `gorm:"primaryKey" json:"id"`
-	CreatedAt             time.Time      `json:"-"`
-	UpdatedAt             time.Time      `json:"-"`
-	DeletedAt             *time.Time     `gorm:"index" json:"-,omitempty"`
-	Claim                 LocationClaim  `gorm:"foreignKey:LocationID" json:"claim"`
-	Address               Address        `gorm:"type:jsonb" json:"address"`
-	Bio                   *string        `json:"bio"`
-	BusinessStatus        BusinessStatus `gorm:"type:text" json:"businessStatus"`
-	EventID               *uint          `gorm:"index" json:"eventId"`
-	Event                 Event          `json:"event"`
-	GoogleID              string         `gorm:"uniqueIndex;not null" json:"googleId"`
-	GoogleMapsUrl         string         `json:"googleMapsUrl"`
-	GoogleRating          float64        `json:"googleRating"`
-	GoogleUserRatingCount int            `json:"googleUserRatingCount"`
-	Hook                  *string        `json:"hook"`
-	Image                 string         `json:"image"`
-	Images                pq.StringArray `gorm:"type:varchar[]" json:"images"`
-	IsClaimed             bool           `json:"isClaimed"`
-	Lat                   float64        `json:"lat"`
-	Lng                   float64        `json:"lng"`
-	LocationTags          []LocationTag  `gorm:"many2many:location_location_tags" json:"locationTags"`
-	PlaceID               uint           `gorm:"index" json:"placeId"`
-	Place                 Place          `json:"place"`
-	Name                  string         `json:"name"`
-	Notes                 []LocationNote `gorm:"foreignKey:LocationID" json:"notes"`
-	OpeningHours          OpeningHours   `gorm:"type:jsonb" json:"openingHours"`
-	OSMRef                string         `gorm:"uniqueIndex;not null" json:"osmRef"`
-	Phone                 string         `json:"phone"`
-	PriceLevel            PriceLevel     `json:"priceLevel"`
-	Score                 float64        `json:"score"`
-	TripadvisorRating     ExternalRating `gorm:"type:jsonb" json:"tripadvisorRating"`
-	GooglePlacesRating    ExternalRating `gorm:"type:jsonb" json:"googlePlacesRating"`
-	WebsiteUrl            string         `json:"websiteUrl"`
-	Email                 string         `json:"email"`
-	ReviewSummary         string         `json:"reviewSummary"`
+	ID                    uint                  `gorm:"primaryKey" json:"id"`
+	CreatedAt             time.Time             `json:"-"`
+	UpdatedAt             time.Time             `json:"-"`
+	DeletedAt             *time.Time            `gorm:"index" json:"-,omitempty"`
+	AccountRoles          []AccountLocationRole `gorm:"foreignKey:LocationID" json:"accountRoles"`
+	Address               Address               `gorm:"type:jsonb" json:"address"`
+	Bio                   *string               `json:"bio"`
+	BusinessStatus        BusinessStatus        `gorm:"type:text" json:"businessStatus"`
+	Claim                 LocationClaim         `gorm:"foreignKey:LocationID" json:"claim"`
+	EventID               *uint                 `gorm:"index" json:"eventId"`
+	Event                 Event                 `json:"event"`
+	GoogleID              string                `gorm:"uniqueIndex;not null" json:"googleId"`
+	GoogleMapsUrl         string                `json:"googleMapsUrl"`
+	GoogleRating          float64               `json:"googleRating"`
+	GoogleUserRatingCount int                   `json:"googleUserRatingCount"`
+	Hook                  *string               `json:"hook"`
+	Image                 string                `json:"image"`
+	Images                pq.StringArray        `gorm:"type:varchar[]" json:"images"`
+	IsClaimed             bool                  `json:"isClaimed"`
+	Lat                   float64               `json:"lat"`
+	Lng                   float64               `json:"lng"`
+	LocationTags          []LocationTag         `gorm:"many2many:location_location_tags" json:"locationTags"`
+	PlaceID               uint                  `gorm:"index" json:"placeId"`
+	Place                 Place                 `json:"place"`
+	Name                  string                `json:"name"`
+	Notes                 []LocationNote        `gorm:"foreignKey:LocationID" json:"notes"`
+	OpeningHours          OpeningHours          `gorm:"type:jsonb" json:"openingHours"`
+	OSMRef                string                `gorm:"uniqueIndex;not null" json:"osmRef"`
+	Phone                 string                `json:"phone"`
+	PriceLevel            PriceLevel            `json:"priceLevel"`
+	Score                 float64               `json:"score"`
+	TripadvisorRating     ExternalRating        `gorm:"type:jsonb" json:"tripadvisorRating"`
+	GooglePlacesRating    ExternalRating        `gorm:"type:jsonb" json:"googlePlacesRating"`
+	WebsiteUrl            string                `json:"websiteUrl"`
+	Email                 string                `json:"email"`
+	ReviewSummary         string                `json:"reviewSummary"`
 }
 
 // LocationDTO
