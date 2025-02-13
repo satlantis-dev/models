bf57c89d85d30a46376b799a49bfd1a33cf05b61
noib <b_n_79@protonmail.com>
2025-02-14 02:19:17 +0800

    Create gallery image tables

---
+++ place_gallery_image.go
@@ -0,0 +1,13 @@
+package models
+
+import "time"
+
+type PlaceGalleryImage struct {
+	ID        uint      `gorm:"primaryKey" json:"id"`
+	PlaceID   uint      `json:"placeId"`
+	Place     *Place    `gorm:"foreignKey:PlaceID" json:"place,omitempty"`
+	Url       string    `json:"url"`
+	Caption   *string   `json:"caption"`
+	Source    string    `json:"source"`
+	CreatedAt time.Time `json:"createdAt"`
+}
