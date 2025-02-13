6d7e9bd289da65ef085cba14096c20487bd560d9
noib <b_n_79@protonmail.com>
2025-02-14 02:37:01 +0800

    Add image categories

--- location_gallery_image.go
+++ location_gallery_image.go
@@ -2,13 +2,23 @@ package models
 
 import "time"
 
+type ImageCategory string
+
+const (
+	ImageCategoryGeneral  ImageCategory = "general"
+	ImageCategoryMenu     ImageCategory = "menu"
+	ImageCategoryExterior ImageCategory = "exterior"
+	ImageCategoryInterior ImageCategory = "interior"
+	ImageCategoryFood     ImageCategory = "food"
+)
+
 type LocationGalleryImage struct {
-	ID         uint      `gorm:"primaryKey" json:"id"`
-	LocationID uint      `json:"locationId"`
-	Location   *Location `gorm:"foreignKey:LocationID" json:"location,omitempty"`
-	Url        string    `json:"url"`
-	Caption    *string   `json:"caption"`
-	Category   *string   `json:"category"`
-	Source     string    `json:"source"`
-	CreatedAt  time.Time `json:"createdAt"`
+	ID         uint          `gorm:"primaryKey" json:"id"`
+	LocationID uint          `json:"locationId"`
+	Location   *Location     `gorm:"foreignKey:LocationID" json:"location,omitempty"`
+	Url        string        `json:"url"`
+	Caption    *string       `json:"caption"`
+	Category   ImageCategory `gorm:"type:string,default:general" json:"category"`
+	Source     string        `json:"source"`
+	CreatedAt  time.Time     `json:"createdAt"`
 }
