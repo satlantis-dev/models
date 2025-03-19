fa3cf5a1f067691e6635d2f22931f0d9cdc30771
noib <b_n_79@protonmail.com>
2025-03-20 00:24:31 +0800

    Change Categories type to pq.Int32Array

--- interest.go
+++ interest.go
@@ -19,18 +19,18 @@ const (
 )
 
 type Interest struct {
-	ID                    uint               `gorm:"primaryKey" json:"id"`
-	Name                  string             `gorm:"unique" json:"name"`
-	CreatedAt             time.Time          `json:"-"`
-	UpdatedAt             time.Time          `json:"-"`
-	DeletedAt             *time.Time         `gorm:"index" json:"-,omitempty"`
-	Description           string             `gorm:"type:text" json:"description"`
-	RecommendationsByNpub pq.StringArray     `gorm:"type:varchar[]" json:"recommendationsByNpub"`
-	RecommendationsById   pq.Int32Array      `gorm:"type:integer[]" json:"recommendationsById"`
-	AutofollowsByNpub     pq.StringArray     `gorm:"type:varchar[]" json:"autofollowsByNpub"`
-	AutofollowsById       pq.Int32Array      `gorm:"type:integer[]" json:"autofollowsById"`
-	Hashtags              pq.StringArray     `gorm:"type:varchar[]" json:"hashtags"`
-	LocationTags          []LocationTag      `gorm:"many2many:interest_location_tags" json:"locationTags"`
-	PrimaryCategory       InterestCategory   `json:"primaryCategory"`
-	Categories            []InterestCategory `gorm:"type:integer[]" json:"categories"`
+	ID                    uint             `gorm:"primaryKey" json:"id"`
+	Name                  string           `gorm:"unique" json:"name"`
+	CreatedAt             time.Time        `json:"-"`
+	UpdatedAt             time.Time        `json:"-"`
+	DeletedAt             *time.Time       `gorm:"index" json:"-,omitempty"`
+	Description           string           `gorm:"type:text" json:"description"`
+	RecommendationsByNpub pq.StringArray   `gorm:"type:varchar[]" json:"recommendationsByNpub"`
+	RecommendationsById   pq.Int32Array    `gorm:"type:integer[]" json:"recommendationsById"`
+	AutofollowsByNpub     pq.StringArray   `gorm:"type:varchar[]" json:"autofollowsByNpub"`
+	AutofollowsById       pq.Int32Array    `gorm:"type:integer[]" json:"autofollowsById"`
+	Hashtags              pq.StringArray   `gorm:"type:varchar[]" json:"hashtags"`
+	LocationTags          []LocationTag    `gorm:"many2many:interest_location_tags" json:"locationTags"`
+	PrimaryCategory       InterestCategory `json:"primaryCategory"`
+	Categories            pq.Int32Array    `gorm:"type:integer[]" json:"categories"`
 }
