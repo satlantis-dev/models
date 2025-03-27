e8a9221505c94775694dacc9fa3859601154dea4
Vano Khuroshvili <vano.khuroshvili@gmail.com>
2025-03-27 06:04:00 +0400

    Add `otpAttempts` field to VerificationToken table

--- verification_token.go
+++ verification_token.go
@@ -5,11 +5,12 @@ import (
 )
 
 type VerificationToken struct {
-	ID        uint       `gorm:"primaryKey" json:"id"`
-	CreatedAt time.Time  `json:"-"`
-	UpdatedAt time.Time  `json:"-"`
-	DeletedAt *time.Time `gorm:"index" json:"-,omitempty"`
-	Token     string     `gorm:"type:text" json:"token"`
-	AccountID uint       `gorm:"index" json:"accountId"`
-	OTPHash   string     `gorm:"type:text" json:"otpHash"`
+	ID          uint       `gorm:"primaryKey" json:"id"`
+	CreatedAt   time.Time  `json:"-"`
+	UpdatedAt   time.Time  `json:"-"`
+	DeletedAt   *time.Time `gorm:"index" json:"-,omitempty"`
+	Token       string     `gorm:"type:text" json:"token"`
+	AccountID   uint       `gorm:"index" json:"accountId"`
+	OTPHash     string     `gorm:"type:text" json:"otpHash"`
+	OTPAttempts uint       `gorm:"default:0" json:"otpAttempts"`
 }
