7081df741fa60ac1352ff062233cf5aa0b9c1409
noib <b_n_79@protonmail.com>
2025-04-16 23:33:10 +0800

    Add flag for focus account notes

--- note_ranking.go
+++ note_ranking.go
@@ -3,13 +3,14 @@ package models
 import "time"
 
 type NoteRanking struct {
-	NoteId      uint      `gorm:"primaryKey" json:"noteId"`
-	Note        Note      `gorm:"foreignKey:NoteId" json:"note"`
-	OnSatlantis bool      `json:"onSatlantis"`
-	Nsfw        bool      `json:"nsfw"`
-	Reactions   int       `gorm:"default:0" json:"reactions"`
-	Replies     int       `gorm:"default:0" json:"replies"`
-	AllReplies  int       `gorm:"default:0" json:"allReplies"`
-	Score       float64   `gorm:"default:0" json:"score"`
-	Timestamp   time.Time `json:"timestamp"`
+	NoteId           uint      `gorm:"primaryKey" json:"noteId"`
+	Note             Note      `gorm:"foreignKey:NoteId" json:"note"`
+	OnSatlantis      bool      `json:"onSatlantis"`
+	FromFocusAccount bool      `json:"fromFocusAccount"`
+	Nsfw             bool      `json:"nsfw"`
+	Reactions        int       `gorm:"default:0" json:"reactions"`
+	Replies          int       `gorm:"default:0" json:"replies"`
+	AllReplies       int       `gorm:"default:0" json:"allReplies"`
+	Score            float64   `gorm:"default:0" json:"score"`
+	Timestamp        time.Time `json:"timestamp"`
 }
