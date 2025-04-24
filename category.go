027bf059dab7669a5f95d5c145e4dcbebf04e7fb
noib <b_n_79@protonmail.com>
2025-04-24 23:13:14 +0800

    Add category weights

--- category.go
+++ category.go
@@ -4,4 +4,5 @@ type Category struct {
 	ID          uint   `gorm:"primaryKey" json:"id"`
 	Description string `gorm:"type:text" json:"description"`
 	Name        string `gorm:"type:text" json:"name"`
+	Weight      uint   `json:"weight"`
 }
