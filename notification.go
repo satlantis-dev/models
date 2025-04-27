411ec9223dc1d559af7258517e760af270e40be8
Vano Khuroshvili <vano.khuroshvili@gmail.com>
2025-04-28 03:07:08 +0400

    Add `payload` field to notifications

--- notification.go
+++ notification.go
@@ -5,16 +5,17 @@ import (
 )
 
 type Notification struct {
-	ID                 uint       `gorm:"primaryKey" json:"id"`
-	CreatorAccountID   uint       `gorm:"index" json:"creatorAccountId"` // Account that created the event
-	CreatorAccount     AccountDTO `gorm:"foreignKey:CreatorAccountID;references:ID" json:"creatorAccount"`
-	RecipientAccountID uint       `gorm:"index" json:"-"` // Account that will be notified
-	Type               string     `gorm:"type:varchar(255)" json:"type"`
-	Action             string     `gorm:"type:text" json:"action"`
-	ImageURL           string     `gorm:"type:text" json:"imageUrl"`
-	Link               string     `gorm:"type:text" json:"link"`
-	Message            string     `gorm:"type:text" json:"message"`
-	IsRead             bool       `gorm:"default:false" json:"isRead"`
-	CreatedAt          time.Time  `json:"createdAt"`
-	UpdatedAt          time.Time  `json:"-"`
+	ID                 uint                    `gorm:"primaryKey" json:"id"`
+	CreatorAccountID   uint                    `gorm:"index" json:"creatorAccountId"` // Account that created the event
+	CreatorAccount     AccountDTO              `gorm:"foreignKey:CreatorAccountID;references:ID" json:"creatorAccount"`
+	RecipientAccountID uint                    `gorm:"index" json:"-"` // Account that will be notified
+	Type               string                  `gorm:"type:varchar(255)" json:"type"`
+	Action             string                  `gorm:"type:text" json:"action"`
+	ImageURL           string                  `gorm:"type:text" json:"imageUrl"`
+	Link               string                  `gorm:"type:text" json:"link"`
+	Message            string                  `gorm:"type:text" json:"message"`
+	IsRead             bool                    `gorm:"default:false" json:"isRead"`
+	Payload            *map[string]interface{} `gorm:"type:jsonb;serializer:json" json:"payload,omitempty"`
+	CreatedAt          time.Time               `json:"createdAt"`
+	UpdatedAt          time.Time               `json:"-"`
 }
