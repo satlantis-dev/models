d29ca3493bc79ccf926fbebde34ae06e918d73fd
Vano Khuroshvili <vano.khuroshvili@gmail.com>
2025-05-03 01:03:30 +0400

    Rename chat message field

--- chat_message.go
+++ chat_message.go
@@ -4,7 +4,7 @@ import "time"
 
 type ChatMessage struct {
 	ID             uint         `gorm:"primaryKey" json:"id"`
-	NostrID        string       `gorm:"index" json:"nostrId"`
+	EventID        string       `gorm:"index" json:"eventId"`
 	ConversationID uint         `json:"conversationId"`
 	Conversation   Conversation `gorm:"foreignKey:ConversationID" json:"conversation"`
 	PushSentAt     *time.Time   `json:"pushSentAt"`
