5cb336f50071dfb7206962a7e868816af1ec5dec
Vano Khuroshvili <vano.khuroshvili@gmail.com>
2025-05-16 05:04:29 +0400

    Added NoteRepost model

---
+++ note_repost.go
@@ -0,0 +1,14 @@
+package models
+
+import "time"
+
+type NoteRepost struct {
+	ID        uint       `gorm:"primaryKey" json:"id"`
+	EventID   string     `gorm:"index" json:"eventId"`
+	NoteID    uint       `json:"noteId"`
+	Note      Note       `gorm:"foreignKey:NoteID" json:"note"`
+	AccountID uint       `json:"accountId"`
+	Account   AccountDTO `gorm:"foreignKey:AccountID" json:"account"`
+	CreatedAt time.Time  `json:"-"`
+	DeletedAt *time.Time `json:"-"`
+}
