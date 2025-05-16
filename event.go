bc34390cb82f6b5010b89e85b1571c235cd13baa
Vano Khuroshvili <vano.khuroshvili@gmail.com>
2025-05-16 05:15:06 +0400

    Add TagsRaw field to Event model

--- event.go
+++ event.go
@@ -1,13 +1,16 @@
 package models
 
+import "github.com/nbd-wtf/go-nostr"
+
 type Event struct {
-	ID         uint   `gorm:"primaryKey" json:"id"`
-	NostrID    string `gorm:"uniqueIndex" json:"nostrId"`
-	CreatedAt  int64  `json:"createdAt"`
-	Content    string `gorm:"type:text" json:"content"`
-	Kind       uint   `gorm:"index" json:"kind"`
-	PubKey     string `gorm:"type:text;index" json:"pubkey"`
-	Sig        string `gorm:"type:text" json:"sig"`
-	Tags       []Tag  `gorm:"foreignKey:EventID" json:"tags"`
-	Reconciled bool   `gorm:"default:false" json:"reconciled"`
+	ID         uint       `gorm:"primaryKey" json:"id"`
+	NostrID    string     `gorm:"uniqueIndex" json:"nostrId"`
+	CreatedAt  int64      `json:"createdAt"`
+	Content    string     `gorm:"type:text" json:"content"`
+	Kind       uint       `gorm:"index" json:"kind"`
+	PubKey     string     `gorm:"type:text;index" json:"pubkey"`
+	Sig        string     `gorm:"type:text" json:"sig"`
+	Tags       []Tag      `gorm:"foreignKey:EventID" json:"tags"`
+	TagsRaw    nostr.Tags `gorm:"type:jsonb;serializer:json" json:"tagsData"`
+	Reconciled bool       `gorm:"default:false" json:"reconciled"`
 }
