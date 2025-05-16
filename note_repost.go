cf9ffdefdc085987f78baa96d873d34a014708be
Vano Khuroshvili <vano.khuroshvili@gmail.com>
2025-05-16 06:47:37 +0400

    Use Account instead of AccountDTO

--- note_repost.go
+++ note_repost.go
@@ -8,7 +8,7 @@ type NoteRepost struct {
 	NoteID    uint       `json:"noteId"`
 	Note      Note       `gorm:"foreignKey:NoteID" json:"note"`
 	AccountID uint       `json:"accountId"`
-	Account   AccountDTO `gorm:"foreignKey:AccountID" json:"account"`
+	Account   Account    `gorm:"foreignKey:AccountID" json:"account"`
 	CreatedAt time.Time  `json:"-"`
 	DeletedAt *time.Time `json:"-"`
 }
