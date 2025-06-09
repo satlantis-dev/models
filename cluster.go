1ac24107f4a58e7322112165e84c051c7cedb7d2
noib <b_n_79@protonmail.com>
2025-06-09 17:09:28 +0200

    Add active flag

--- cluster.go
+++ cluster.go
@@ -4,6 +4,7 @@ type Cluster struct {
 	ID        uint   `gorm:"primaryKey" json:"id"`
 	Label     string `gorm:"type:text" json:"label"`
 	UserCount int    `gorm:"default:0" json:"userCount"`
+	Active    bool   `gorm:"default:true" json:"active"`
 }
 
 type ClusterInterest struct {
