aaa839452dd7d59b7d0980ba3157ded93d1fbbd0
Vano Khuroshvili <vano.khuroshvili@gmail.com>
2025-06-09 19:15:41 +0200

    test column

--- chat_message.go
+++ chat_message.go
@@ -9,6 +9,7 @@ type ChatMessage struct {
 	Conversation   Conversation `gorm:"foreignKey:ConversationID" json:"conversation"`
 	PushSentAt     *time.Time   `json:"pushSentAt"`
 	Content        *string      `gorm:"type:text" json:"content"`
+	Content2       *string      `gorm:"type:text" json:"content2"`
 	CreatedAt      time.Time    `json:"-"`
 	DeletedAt      *time.Time   `json:"-"`
 }
