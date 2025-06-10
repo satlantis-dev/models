f746a6a9f55c67fc75d51a8ca6a513bdc45d1038
noib <b_n_79@protonmail.com>
2025-06-10 12:26:02 +0200

    Add BloomFilterCount

--- account_note_tracker.go
+++ account_note_tracker.go
@@ -1,8 +1,9 @@
 package models
 
 type AccountNoteTracker struct {
-	AccountID   uint       `gorm:"index;primaryKey" json:"accountId"`
-	Account     AccountDTO `gorm:"foreignKey:AccountID" json:"account"`
-	BloomFilter []byte     `json:"bloomFilter"`
-	FeedNotes   string     `gorm:"type:jsonb" json:"feedNotes"`
+	AccountID        uint       `gorm:"index;primaryKey" json:"accountId"`
+	Account          AccountDTO `gorm:"foreignKey:AccountID" json:"account"`
+	BloomFilter      []byte     `json:"bloomFilter"`
+	FeedNotes        string     `gorm:"type:jsonb" json:"feedNotes"`
+	BloomFilterCount int        `gorm:"default:0" json:"bloomFilterCount"`
 }
