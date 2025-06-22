23b46a6419973590d258dfd84f57c6e45a2ff5b1
noib <b_n_79@protonmail.com>
2025-06-22 10:42:02 +0800

    Remove DTO

--- collection.go
+++ collection.go
@@ -9,14 +9,14 @@ import (
 type Collection struct {
 	ID          uint           `gorm:"primaryKey" json:"id"`
 	AccountID   uint           `gorm:"index;uniqueIndex:idx_accountid_name" json:"accountId"`
-	Account     AccountDTO     `gorm:"foreignKey:AccountID" json:"account,omitempty"`
+	Account     Account        `gorm:"foreignKey:AccountID" json:"account,omitempty"`
 	Name        string         `gorm:"type:text;not null;uniqueIndex:idx_accountid_name" json:"name"`
 	Description string         `gorm:"type:text" json:"description"`
 	CreatedAt   time.Time      `json:"createdAt"`
 	UpdatedAt   time.Time      `json:"updatedAt"`
 	DeletedAt   gorm.DeletedAt `gorm:"index" json:"-"`
 	IsPublic    bool           `gorm:"default:true" json:"isPublic"`
-	Locations   []LocationDTO  `gorm:"many2many:collection_locations;joinForeignKey:CollectionID;JoinReferences:LocationID" json:"locations,omitempty"`
+	Locations   []Location     `gorm:"many2many:collection_locations;joinForeignKey:CollectionID;JoinReferences:LocationID" json:"locations,omitempty"`
 }
 
 func (Collection) TableName() string {
