0ac8c447c87c0c553751ee8579ad70c78cb9daf3
noib <b_n_79@protonmail.com>
2025-06-22 09:34:01 +0800

    Add unique index to collection on account_id x name

--- collection.go
+++ collection.go
@@ -1,18 +1,26 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Collection struct {
 	ID          uint                 `gorm:"primaryKey" json:"id"`
-	AccountID   uint                 `gorm:"index" json:"accountId"`
+	AccountID   uint                 `gorm:"index;uniqueIndex:idx_accountid_name" json:"accountId"`
 	Account     AccountDTO           `gorm:"foreignKey:AccountID" json:"account"`
-	Name        string               `gorm:"type:text" json:"name"`
+	Name        string               `gorm:"type:text;not null;uniqueIndex:idx_accountid_name" json:"name"`
 	Description string               `gorm:"type:text" json:"description"`
 	CreatedAt   time.Time            `json:"createdAt"`
 	UpdatedAt   time.Time            `json:"updatedAt"`
-	DeletedAt   *time.Time           `gorm:"index" json:"-,omitempty"`
+	DeletedAt   gorm.DeletedAt       `gorm:"index" json:"-"`
 	IsPublic    bool                 `gorm:"default:true" json:"isPublic"`
 	Locations   []CollectionLocation `gorm:"foreignKey:CollectionID" json:"locations,omitempty"`
+}
+
+func (Collection) TableName() string {
+	return "collections"
 }
 
 type CollectionLocation struct {
