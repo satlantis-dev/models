adcaa27f917cc472ae3210a9d5521e4413483bba
noib <b_n_79@protonmail.com>
2025-06-22 10:56:28 +0800

    Auto fill CreatedAt and UpdatedAt

--- collection.go
+++ collection.go
@@ -9,11 +9,11 @@ import (
 type Collection struct {
 	ID          uint           `gorm:"primaryKey" json:"id"`
 	AccountID   uint           `gorm:"index;uniqueIndex:idx_accountid_name" json:"accountId"`
-	Account     Account        `gorm:"foreignKey:AccountID" json:"account,omitempty"`
+	Account     Account        `gorm:"foreignKey:AccountID" json:"account"`
 	Name        string         `gorm:"type:text;not null;uniqueIndex:idx_accountid_name" json:"name"`
 	Description string         `gorm:"type:text" json:"description"`
-	CreatedAt   time.Time      `json:"createdAt"`
-	UpdatedAt   time.Time      `json:"updatedAt"`
+	CreatedAt   time.Time      `gorm:"autoCreateTime" json:"createdAt"`
+	UpdatedAt   time.Time      `gorm:"autoUpdateTime" json:"updatedAt"`
 	DeletedAt   gorm.DeletedAt `gorm:"index" json:"-"`
 	IsPublic    bool           `gorm:"default:true" json:"isPublic"`
 	Locations   []Location     `gorm:"many2many:collection_locations;joinForeignKey:CollectionID;JoinReferences:LocationID" json:"locations,omitempty"`
