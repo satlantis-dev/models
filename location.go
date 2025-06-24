40be6f59946527f68ddc3a39b3d0c47bf7991c1f
noib <b_n_79@protonmail.com>
2025-06-25 00:10:43 +0800

    Change distance to int

--- location.go
+++ location.go
@@ -268,12 +268,5 @@ func (l Location) ToDTO(db *gorm.DB) (*LocationDTO, error) {
 
 type NearbyLocationDTO struct {
 	LocationDTO
-	Distance float64 `json:"distance"`
-}
-
-func ToNearbyLocationDTO(dto LocationDTO, distance float64) NearbyLocationDTO {
-	return NearbyLocationDTO{
-		LocationDTO: dto,
-		Distance:    distance,
-	}
+	Distance int `json:"distance"`
 }
