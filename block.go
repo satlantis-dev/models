a1eb5955316a72692f182be5cef78f8ff68347d6
noib <b_n_79@protonmail.com>
2025-07-01 02:05:19 +0800

    Tidy up

--- block.go
+++ block.go
@@ -6,5 +6,5 @@ type Block struct {
 	BlockerID     uint      `gorm:"index:idx_blocker_blocking,unique"`
 	BlockingID    uint      `gorm:"index:idx_blocker_blocking,unique"`
 	CreatedAt     time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"-"`
-	Bidirectional bool      `gorm:"default:false" json:"bidirectional,omitempty"` // Bidirectional -> block, Unidirectional -> mute
+	Bidirectional bool      `gorm:"default:false" json:"bidirectional"` // Bidirectional -> block, Unidirectional -> mute
 }
