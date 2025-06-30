a47e69134ed029809f643ed0b37fb3891b3a3be3
noib <b_n_79@protonmail.com>
2025-07-01 02:04:53 +0800

    Allow unidirectional block (i.e. mute)

--- block.go
+++ block.go
@@ -3,7 +3,8 @@ package models
 import "time"
 
 type Block struct {
-	BlockerID  uint      `gorm:"index:idx_blocker_blocking,unique"`
-	BlockingID uint      `gorm:"index:idx_blocker_blocking,unique"`
-	CreatedAt  time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"-"`
+	BlockerID     uint      `gorm:"index:idx_blocker_blocking,unique"`
+	BlockingID    uint      `gorm:"index:idx_blocker_blocking,unique"`
+	CreatedAt     time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"-"`
+	Bidirectional bool      `gorm:"default:false" json:"bidirectional,omitempty"` // Bidirectional -> block, Unidirectional -> mute
 }
