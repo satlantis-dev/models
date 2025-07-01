bc329c870a8024a5ca65849889156eb2ddb08789
noib <b_n_79@protonmail.com>
2025-07-01 12:20:28 +0800

    Add constraints

--- block.go
+++ block.go
@@ -3,8 +3,8 @@ package models
 import "time"
 
 type Block struct {
-	BlockerID     uint      `gorm:"index:idx_blocker_blocking,unique"`
-	BlockingID    uint      `gorm:"index:idx_blocker_blocking,unique"`
+	BlockerID     uint      `gorm:"primaryKey;index:idx_blocker_blocking,unique;uniqueIndex:idx_blockerid"`
+	BlockingID    uint      `gorm:"primaryKey;index:idx_blocker_blocking,unique;uniqueIndex:idx_blockingid"`
 	CreatedAt     time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"-"`
 	Bidirectional bool      `gorm:"default:false" json:"bidirectional"` // Bidirectional -> block, Unidirectional -> mute
 }
