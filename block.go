796141879a8af48dcf8087f5fd48604f3c7702b6
noib <b_n_79@protonmail.com>
2025-07-01 12:23:41 +0800

    Change unique constraint

--- block.go
+++ block.go
@@ -3,8 +3,8 @@ package models
 import "time"
 
 type Block struct {
-	BlockerID     uint      `gorm:"primaryKey;index:idx_blocker_blocking,unique;uniqueIndex:idx_blockerid"`
-	BlockingID    uint      `gorm:"primaryKey;index:idx_blocker_blocking,unique;uniqueIndex:idx_blockingid"`
+	BlockerID     uint      `gorm:"primaryKey;index:idx_blocker_blocking,unique;index:idx_blockerid"`
+	BlockingID    uint      `gorm:"primaryKey;index:idx_blocker_blocking,unique;index:idx_blockingid"`
 	CreatedAt     time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"-"`
 	Bidirectional bool      `gorm:"default:false" json:"bidirectional"` // Bidirectional -> block, Unidirectional -> mute
 }
