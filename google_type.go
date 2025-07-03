cdb77854e353bb76ff073477c4c48c16952e064f
noib <b_n_79@protonmail.com>
2025-07-03 15:39:15 +0800

    Add Scan and Value interfaces

--- google_type.go
+++ google_type.go
@@ -1,9 +1,42 @@
 package models
 
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"errors"
+)
+
+type JSONBMap map[string]string
+
+func (m JSONBMap) Value() (driver.Value, error) {
+	if m == nil {
+		return "{}", nil
+	}
+	b, err := json.Marshal(m)
+	return string(b), err
+}
+
+func (m *JSONBMap) Scan(value interface{}) error {
+	if value == nil {
+		*m = make(JSONBMap)
+		return nil
+	}
+	var bytes []byte
+	switch v := value.(type) {
+	case string:
+		bytes = []byte(v)
+	case []byte:
+		bytes = v
+	default:
+		return errors.New("unsupported type for MapStringString")
+	}
+	return json.Unmarshal(bytes, m)
+}
+
 type GoogleType struct {
-	Name        string             `gorm:"primaryKey" json:"name"`
-	OSMStdTag   *map[string]string `gorm:"type:jsonb" json:"osmStdTag"`
-	OSMExtraTag *map[string]string `gorm:"type:jsonb" json:"osmExtraTag"`
+	Name        string    `gorm:"primaryKey" json:"name"`
+	OSMStdTag   *JSONBMap `gorm:"type:jsonb" json:"osmStdTag"`
+	OSMExtraTag *JSONBMap `gorm:"type:jsonb" json:"osmExtraTag"`
 }
 
 func (GoogleType) TableName() string {
