ca9ae5945146035dbd5a8719788bcd1b29a36c11
noib <b_n_79@protonmail.com>
2025-07-10 15:23:29 +0800

    Add NumLocations to Collection

--- collection.go
+++ collection.go
@@ -7,17 +7,18 @@ import (
 )
 
 type Collection struct {
-	ID          uint                 `gorm:"primaryKey" json:"id"`
-	AccountID   uint                 `gorm:"index;uniqueIndex:idx_accountid_name" json:"accountId"`
-	Account     *Account             `gorm:"foreignKey:AccountID" json:"account,omitempty"`
-	Name        string               `gorm:"type:text;not null;uniqueIndex:idx_accountid_name" json:"name"`
-	Description *string              `gorm:"type:text" json:"description,omitempty"`
-	Cover       *string              `gorm:"type:text" json:"cover,omitempty"`
-	CreatedAt   time.Time            `gorm:"autoCreateTime" json:"createdAt"`
-	UpdatedAt   time.Time            `gorm:"autoUpdateTime" json:"updatedAt"`
-	DeletedAt   gorm.DeletedAt       `gorm:"index" json:"-"`
-	IsPublic    bool                 `gorm:"default:true" json:"isPublic"`
-	Locations   []CollectionLocation `json:"locations"`
+	ID           uint                 `gorm:"primaryKey" json:"id"`
+	AccountID    uint                 `gorm:"index;uniqueIndex:idx_accountid_name" json:"accountId"`
+	Account      *Account             `gorm:"foreignKey:AccountID" json:"account,omitempty"`
+	Name         string               `gorm:"type:text;not null;uniqueIndex:idx_accountid_name" json:"name"`
+	Description  *string              `gorm:"type:text" json:"description,omitempty"`
+	Cover        *string              `gorm:"type:text" json:"cover,omitempty"`
+	CreatedAt    time.Time            `gorm:"autoCreateTime" json:"createdAt"`
+	UpdatedAt    time.Time            `gorm:"autoUpdateTime" json:"updatedAt"`
+	DeletedAt    gorm.DeletedAt       `gorm:"index" json:"-"`
+	IsPublic     bool                 `gorm:"default:true" json:"isPublic"`
+	Locations    []CollectionLocation `json:"locations"`
+	NumLocations int                  `gorm:"-" json:"numLocations"`
 }
 
 func (Collection) TableName() string {
