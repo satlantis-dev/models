7793a2319e095600fdc45b9b78abf1d98776a4ec
noib <b_n_79@protonmail.com>
2025-07-10 09:22:48 +0800

    Add collection id to reviews

--- account_location_review.go
+++ account_location_review.go
@@ -1,12 +1,14 @@
 package models
 
 type AccountLocationReview struct {
-	AccountID  uint         `gorm:"primaryKey;index" json:"accountId"`
-	Account    AccountDTO   `gorm:"foreignKey:AccountID" json:"account,omitempty"`
-	LocationID uint         `gorm:"primaryKey;index" json:"locationId"`
-	Location   *LocationDTO `gorm:"foreignKey:LocationID" json:"location,omitempty"`
-	ReviewText string       `json:"reviewText"`
-	IsPositive bool         `gorm:"not null;default:true" json:"isPositive"`
+	AccountID    uint         `gorm:"primaryKey;index" json:"accountId"`
+	Account      AccountDTO   `gorm:"foreignKey:AccountID" json:"account,omitempty"`
+	LocationID   uint         `gorm:"primaryKey;index" json:"locationId"`
+	Location     *LocationDTO `gorm:"foreignKey:LocationID" json:"location,omitempty"`
+	CollectionID *uint        `gorm:"primaryKey;index" json:"collectionId"`
+	Collection   *Collection  `gorm:"foreignKey:CollectionID" json:"collection,omitempty"`
+	ReviewText   string       `json:"reviewText"`
+	IsPositive   bool         `gorm:"not null;default:true" json:"isPositive"`
 }
 
 func (AccountLocationReview) TableName() string {
