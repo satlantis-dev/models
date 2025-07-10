3a09ef7460f1c19d9fd8adccd19ebe4423640bbb
noib <b_n_79@protonmail.com>
2025-07-10 10:06:19 +0800

    Fix foreign key

--- collection.go
+++ collection.go
@@ -28,7 +28,7 @@ type CollectionLocation struct {
 	CollectionID   uint               `gorm:"primaryKey;autoIncrement:false;not null;uniqueIndex:idx_collectionid_locationid" json:"collectionId"`
 	Collection     *Collection        `gorm:"foreignKey:CollectionID;references:ID" json:"collection,omitempty"`
 	GoogleID       string             `gorm:"primaryKey;type:text" json:"googleId"`
-	SourceLocation *SourceLocationDTO `gorm:"foreignKey:GoogleID;references:GoogleID" json:"sourceLocation,omitempty"`
+	SourceLocation *SourceLocationDTO `gorm:"foreignKey:GoogleID;references:GoogleId" json:"sourceLocation,omitempty"`
 	Location       *LocationDTO       `gorm:"foreignKey:GoogleID;references:GoogleID" json:"location,omitempty"`
 	SeqNum         int                `gorm:"default:0;not null" json:"seqNum"`
 	Blurb          *string            `gorm:"type:text" json:"blurb,omitempty"`
