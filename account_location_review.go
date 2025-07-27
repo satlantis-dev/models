44e027673d2dee8ee56fd2b3391fa7e95cce010f
noib <b_n_79@protonmail.com>
2025-07-27 15:56:09 +0800

    Add price level and tags in reviews

--- account_location_review.go
+++ account_location_review.go
@@ -10,13 +10,15 @@ type AccountLocationReview struct {
 	ID         uint           `gorm:"primaryKey;autoIncrement" json:"id"`
 	AccountID  uint           `gorm:"uniqueIndex:idx_account_location" json:"accountId"`
 	Account    AccountDTO     `gorm:"foreignKey:AccountID" json:"account,omitempty"`
-	LocationID uint           `gorm:"uniqueIndex:idx_account_location" json:"locationId"`
+	GoogleID   uint           `gorm:"uniqueIndex:idx_account_location" json:"googleId"`
 	Location   *LocationDTO   `gorm:"foreignKey:LocationID" json:"location,omitempty"`
 	ReviewText string         `json:"reviewText"`
 	IsPositive bool           `gorm:"not null;default:true" json:"isPositive"`
 	CreatedAt  time.Time      `gorm:"autoCreateTime" json:"createdAt"`
 	UpdatedAt  time.Time      `gorm:"autoUpdateTime" json:"updatedAt"`
 	DeletedAt  gorm.DeletedAt `gorm:"index" json:"deletedAt,omitempty"`
+	PriceLevel PriceLevel     `gorm:"default:PRICE_LEVEL_UNSPECIFIED" json:"priceLevel"`
+	Tags       JSONBMapSlice  `gorm:"type:jsonb" json:"tags"`
 }
 
 func (AccountLocationReview) TableName() string {
