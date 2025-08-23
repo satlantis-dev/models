0bef83c81aae39fe7a7cd02c1ad81b8a6b51ed40
Vano Khuroshvili <vano.khuroshvili@gmail.com>
2025-08-24 03:06:54 +0400

    Update calendar event rsvp model

--- calendar_event_rsvp.go
+++ calendar_event_rsvp.go
@@ -10,24 +10,31 @@ var (
 	RsvpDeclined   RsvpStatus = "declined"
 	RsvpWaitlisted RsvpStatus = "waitlisted"
 	RsvpPending    RsvpStatus = "pending"
+	RsvpRejected   RsvpStatus = "rejected"
 )
 
 type CalendarEventRSVP struct {
-	ID                   uint      `gorm:"primaryKey" json:"id"`
-	AccountID            uint      `json:"accountId"`
-	Account              Account   `json:"account"`
-	CreatedAt            time.Time `json:"createdAt"`
-	Content              string    `gorm:"type:text" json:"content"`
-	EventID              uint      `gorm:"index" json:"eventId"`
-	Kind                 uint      `gorm:"index" json:"kind"`
-	NostrID              string    `gorm:"index" json:"nostrId"`
-	PubKey               string    `gorm:"type:text;index" json:"pubkey"`
-	Sig                  string    `gorm:"type:text" json:"sig"`
-	Tags                 string    `gorm:"type:jsonb" json:"tags"`
-	CalendarEventID      uint      `json:"calendarEventId"`
-	Status               string    `json:"status"`
-	NotificationWeekSent bool      `gorm:"default:false"`
-	NotificationDaySent  bool      `gorm:"default:false"`
-	NotificationHourSent bool      `gorm:"default:false"`
-	IsSatlantisCreated   bool      `gorm:"default:false"`
+	ID                 uint       `gorm:"primaryKey" json:"id"`
+	AccountID          uint       `json:"accountId"`
+	Account            Account    `json:"account"`
+	CreatedAt          time.Time  `json:"createdAt"`
+	CalendarEventID    uint       `json:"calendarEventId"`
+	Status             string     `json:"status"`
+	AcceptedAt         *time.Time `json:"acceptedAt,omitempty"`
+	RejectedAt         *time.Time `json:"rejectedAt,omitempty"`
+	IsSatlantisCreated bool       `gorm:"default:false"`
+
+	// Nostr fields
+	Content string `gorm:"type:text" json:"content"`
+	EventID uint   `gorm:"index" json:"eventId"`
+	Kind    uint   `gorm:"index" json:"kind"`
+	NostrID string `gorm:"index" json:"nostrId"`
+	PubKey  string `gorm:"type:text;index" json:"pubkey"`
+	Sig     string `gorm:"type:text" json:"sig"`
+	Tags    string `gorm:"type:jsonb" json:"tags"`
+
+	// Notification flags
+	NotificationWeekSent bool `gorm:"default:false"`
+	NotificationDaySent  bool `gorm:"default:false"`
+	NotificationHourSent bool `gorm:"default:false"`
 }
