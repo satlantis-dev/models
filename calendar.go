f343ca44866dc78b99b41e6ba6690636a64a117a
noib <b_n_79@protonmail.com>
2025-08-27 23:20:28 +0800

    Add missing fields

--- calendar.go
+++ calendar.go
@@ -1,11 +1,21 @@
 package models
 
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
 type Calendar struct {
 	ID          uint            `json:"id"`
 	Name        string          `json:"name"`
 	Description string          `json:"description"`
+	Slug        string          `gorm:"size:70" json:"slug"`
 	Banner      string          `json:"banner"`
 	AccountID   uint            `json:"account_id"`
 	Account     *AccountDTO     `gorm:"foreignKey:AccountID" json:"account,omitempty"`
 	Events      []CalendarEvent `gorm:"many2many:calendar_events;" json:"events"`
+	CreatedAt   time.Time       `gorm:"autoCreateTime" json:"createdAt"`
+	UpdatedAt   time.Time       `gorm:"autoUpdateTime" json:"updatedAt"`
+	DeletedAt   *gorm.DeletedAt `gorm:"index" json:"-"`
 }
