08da2f1501f5af33e774b8d1ccbda94b747255d7
noib <b_n_79@protonmail.com>
2025-08-29 14:34:51 +0800

    Fix OnDelete

--- place_calendar_event.go
+++ place_calendar_event.go
@@ -3,12 +3,12 @@ package models
 import "time"
 
 type PlaceCalendarEvent struct {
-	ID              uint          `gorm:"primaryKey" json:"id"`
-	CreatedAt       time.Time     `json:"createdAt"`
-	UpdatedAt       time.Time     `json:"-"`
-	DeletedAt       *time.Time    `gorm:"index" json:"-,omitempty"`
-	PlaceID         uint          `gorm:"index;constraint:OnDelete:CASCADE;" json:"placeId"`
-	Place           Place         `json:"place,omitempty"`
-	CalendarEventID uint          `gorm:"index;constraint:OnDelete:CASCADE;" json:"calendarEventId"`
-	CalendarEvent   CalendarEvent `json:"calendarEvent,omitempty"`
+	ID              uint           `gorm:"primaryKey" json:"id"`
+	CreatedAt       time.Time      `json:"createdAt"`
+	UpdatedAt       time.Time      `json:"-"`
+	DeletedAt       *time.Time     `gorm:"index" json:"-,omitempty"`
+	PlaceID         uint           `gorm:"index" json:"placeId"`
+	Place           *Place         `gorm:"foreignKey:PlaceID;constraint:OnDelete:CASCADE;" json:"place,omitempty"`
+	CalendarEventID uint           `gorm:"index" json:"calendarEventId"`
+	CalendarEvent   *CalendarEvent `gorm:"foreignKey:CalendarEventID;constraint:OnDelete:CASCADE;" json:"calendarEvent,omitempty"`
 }
