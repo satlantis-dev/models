610482f82ce835261975d472804406f9f4359add
noib <b_n_79@protonmail.com>
2025-08-29 14:31:10 +0800

    Cascade on deletee

--- place_calendar_event.go
+++ place_calendar_event.go
@@ -7,8 +7,8 @@ type PlaceCalendarEvent struct {
 	CreatedAt       time.Time     `json:"createdAt"`
 	UpdatedAt       time.Time     `json:"-"`
 	DeletedAt       *time.Time    `gorm:"index" json:"-,omitempty"`
-	PlaceID         uint          `gorm:"index" json:"placeId"`
+	PlaceID         uint          `gorm:"index;constraint:OnDelete:CASCADE;" json:"placeId"`
 	Place           Place         `json:"place,omitempty"`
-	CalendarEventID uint          `gorm:"index" json:"calendarEventId"`
+	CalendarEventID uint          `gorm:"index;constraint:OnDelete:CASCADE;" json:"calendarEventId"`
 	CalendarEvent   CalendarEvent `json:"calendarEvent,omitempty"`
 }
