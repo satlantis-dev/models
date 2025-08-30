3745afdfd13675f0a275d4229c975f8c9047c2b9
noib <b_n_79@protonmail.com>
2025-08-30 17:27:01 +0800

    Reformat AccountLocationRole

--- account_location_role.go
+++ account_location_role.go
@@ -16,14 +16,16 @@ const (
 )
 
 type AccountLocationRole struct {
-	AccountID  uint                    `gorm:"primaryKey" json:"accountId"`
-	Account    *Account                `gorm:"foreignKey:AccountID;constraint:OnDelete:CASCADE" json:"account,omitempty"`
-	LocationID uint                    `gorm:"primaryKey" json:"locationId"`
-	Location   *Location               `gorm:"foreignKey:LocationID;constraint:OnDelete:CASCADE" json:"location,omitempty"`
-	Type       AccountLocationRoleType `gorm:"not null" json:"type"`
-	CreatedAt  time.Time               `json:"-"`
-	UpdatedAt  time.Time               `json:"-"`
-	DeletedAt  gorm.DeletedAt          `gorm:"index" json:"-"`
+	AccountID  uint `gorm:"primaryKey" json:"accountId"`
+	LocationID uint `gorm:"primaryKey" json:"locationId"`
+
+	Type      AccountLocationRoleType `gorm:"not null" json:"type"`
+	CreatedAt time.Time               `json:"-"`
+	UpdatedAt time.Time               `json:"-"`
+	DeletedAt gorm.DeletedAt          `gorm:"index" json:"-"`
+
+	Account  *Account  `gorm:"foreignKey:AccountID;constraint:OnDelete:CASCADE" json:"account,omitempty"`
+	Location *Location `gorm:"foreignKey:LocationID;constraint:OnDelete:CASCADE" json:"location,omitempty"`
 }
 
 func (AccountLocationRole) TableName() string {
