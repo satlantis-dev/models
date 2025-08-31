44411a86df954caa76607c6559e83f7f158b1c5e
noib <b_n_79@protonmail.com>
2025-08-31 15:11:56 +0800

    Fix foreign key

--- account_note_tracker.go
+++ account_note_tracker.go
@@ -1,9 +1,9 @@
 package models
 
 type AccountNoteTracker struct {
-	AccountID        uint       `gorm:"index;primaryKey" json:"accountId"`
-	Account          AccountDTO `gorm:"foreignKey:AccountID" json:"account"`
-	BloomFilter      []byte     `json:"bloomFilter"`
-	FeedNotes        string     `gorm:"type:jsonb" json:"feedNotes"`
-	BloomFilterCount int        `gorm:"default:0" json:"bloomFilterCount"`
+	AccountID        uint     `gorm:"index;primaryKey" json:"accountId"`
+	Account          *Account `gorm:"foreignKey:AccountID;constraint:OnDelete:CASCADE;" json:"account"`
+	BloomFilter      []byte   `json:"bloomFilter"`
+	FeedNotes        string   `gorm:"type:jsonb" json:"feedNotes"`
+	BloomFilterCount int      `gorm:"default:0" json:"bloomFilterCount"`
 }
