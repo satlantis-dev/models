a4fc97f867d6ab427546bb8886418ecab3674d8c
noib <b_n_79@protonmail.com>
2025-08-31 15:17:40 +0800

    Add reference

--- account_note_tracker.go
+++ account_note_tracker.go
@@ -1,8 +1,8 @@
 package models
 
 type AccountNoteTracker struct {
-	AccountID        uint     `gorm:"primaryKey;autoIncrement:false" json:"accountId"`
-	Account          *Account `gorm:"foreignKey:AccountID;constraint:OnDelete:CASCADE;" json:"account"`
+	AccountID        uint     `gorm:"primaryKey" json:"accountId"`
+	Account          *Account `gorm:"foreignKey:AccountID;references:ID;constraint:OnDelete:CASCADE;" json:"account"`
 	BloomFilter      []byte   `json:"bloomFilter"`
 	FeedNotes        string   `gorm:"type:jsonb" json:"feedNotes"`
 	BloomFilterCount int      `gorm:"default:0" json:"bloomFilterCount"`
