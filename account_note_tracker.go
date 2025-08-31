09da696eb0cc0efd3af24abfb252c8ae6552d744
noib <b_n_79@protonmail.com>
2025-08-31 15:14:03 +0800

    Add autoIncrement:false

--- account_note_tracker.go
+++ account_note_tracker.go
@@ -1,7 +1,7 @@
 package models
 
 type AccountNoteTracker struct {
-	AccountID        uint     `gorm:"index;primaryKey" json:"accountId"`
+	AccountID        uint     `gorm:"primaryKey;autoIncrement:false" json:"accountId"`
 	Account          *Account `gorm:"foreignKey:AccountID;constraint:OnDelete:CASCADE;" json:"account"`
 	BloomFilter      []byte   `json:"bloomFilter"`
 	FeedNotes        string   `gorm:"type:jsonb" json:"feedNotes"`
