1b9c022e9084ba9a77e20ee55c7b3d83845f6d00
noib <b_n_79@protonmail.com>
2025-08-31 15:24:03 +0800

    Remove primaryKey

--- account_note_tracker.go
+++ account_note_tracker.go
@@ -1,8 +1,8 @@
 package models
 
 type AccountNoteTracker struct {
-	AccountID        uint     `gorm:"primaryKey;not null;unique" json:"accountId"`
-	Account          *Account `gorm:"foreignKey:AccountID;references:ID;constraint:OnDelete:CASCADE;" json:"account"`
+	AccountID        uint     `gorm:"not null;unique" json:"accountId"`
+	Account          *Account `gorm:"foreignKey:AccountID;references:ID;constraint:OnDelete:CASCADE" json:"account"`
 	BloomFilter      []byte   `json:"bloomFilter"`
 	FeedNotes        string   `gorm:"type:jsonb" json:"feedNotes"`
 	BloomFilterCount int      `gorm:"default:0" json:"bloomFilterCount"`
