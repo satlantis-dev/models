098446e8f1b11a915f3e826e53fd57a6720dd44a
noib <b_n_79@protonmail.com>
2025-08-31 15:26:33 +0800

    Add index

--- account_note_tracker.go
+++ account_note_tracker.go
@@ -1,7 +1,7 @@
 package models
 
 type AccountNoteTracker struct {
-	AccountID        uint     `gorm:"not null;unique" json:"accountId"`
+	AccountID        uint     `gorm:"not null;unique;index" json:"accountId"`
 	Account          *Account `gorm:"foreignKey:AccountID;references:ID;constraint:OnDelete:CASCADE" json:"account"`
 	BloomFilter      []byte   `json:"bloomFilter"`
 	FeedNotes        string   `gorm:"type:jsonb" json:"feedNotes"`
