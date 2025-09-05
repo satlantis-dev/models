3fe86289703262ca3dcde3873b097affdeff570f
noib <b_n_79@protonmail.com>
2025-09-05 16:22:51 +0800

    Omit empty Collection.Locations

--- collection.go
+++ collection.go
@@ -18,7 +18,7 @@ type Collection struct {
 	UpdatedAt    time.Time             `gorm:"autoUpdateTime" json:"updatedAt"`
 	DeletedAt    *gorm.DeletedAt       `gorm:"index" json:"-"`
 	IsPublic     bool                  `gorm:"default:true" json:"isPublic"`
-	Locations    *[]CollectionLocation `gorm:"foreignKey:CollectionID;constraint:OnDelete:CASCADE" json:"locations"`
+	Locations    *[]CollectionLocation `gorm:"foreignKey:CollectionID;constraint:OnDelete:CASCADE" json:"locations,omitempty"`
 	NumLocations int                   `gorm:"-" json:"numLocations"`
 	NumSaves     int                   `gorm:"-" json:"numSaves"`
 	Contributors []AccountMiniDTO      `gorm:"-" json:"contributors,omitempty"`
