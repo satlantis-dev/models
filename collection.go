4970a34f4c0a6e46b55cd6f17e2c29e44ff029b5
noib <b_n_79@protonmail.com>
2025-09-05 16:12:02 +0800

    Change Collection.Locations to pointer

--- collection.go
+++ collection.go
@@ -8,23 +8,23 @@ import (
 )
 
 type Collection struct {
-	ID           uint                 `gorm:"primaryKey" json:"id"`
-	AccountID    uint                 `gorm:"index:idx_accountid_name" json:"accountId"`
-	Account      *Account             `gorm:"foreignKey:AccountID" json:"account,omitempty"`
-	Name         string               `gorm:"type:text;not null;index:idx_accountid_name" json:"name"`
-	Description  *string              `gorm:"type:text" json:"description,omitempty"`
-	Cover        *string              `gorm:"type:text" json:"cover,omitempty"`
-	CreatedAt    time.Time            `gorm:"autoCreateTime" json:"createdAt"`
-	UpdatedAt    time.Time            `gorm:"autoUpdateTime" json:"updatedAt"`
-	DeletedAt    *gorm.DeletedAt      `gorm:"index" json:"-"`
-	IsPublic     bool                 `gorm:"default:true" json:"isPublic"`
-	Locations    []CollectionLocation `gorm:"foreignKey:CollectionID;constraint:OnDelete:CASCADE" json:"locations"`
-	NumLocations int                  `gorm:"-" json:"numLocations"`
-	NumSaves     int                  `gorm:"-" json:"numSaves"`
-	Contributors []AccountMiniDTO     `gorm:"-" json:"contributors,omitempty"`
-	LocationTags []LocationTag        `gorm:"many2many:collection_location_tags;constraint:OnDelete:CASCADE" json:"locationTags"`
-	PlacesByID   pq.Int32Array        `gorm:"type:integer[]" json:"placesById"`
-	Featured     bool                 `gorm:"default:false" json:"featured"`
+	ID           uint                  `gorm:"primaryKey" json:"id"`
+	AccountID    uint                  `gorm:"index:idx_accountid_name" json:"accountId"`
+	Account      *Account              `gorm:"foreignKey:AccountID" json:"account,omitempty"`
+	Name         string                `gorm:"type:text;not null;index:idx_accountid_name" json:"name"`
+	Description  *string               `gorm:"type:text" json:"description,omitempty"`
+	Cover        *string               `gorm:"type:text" json:"cover,omitempty"`
+	CreatedAt    time.Time             `gorm:"autoCreateTime" json:"createdAt"`
+	UpdatedAt    time.Time             `gorm:"autoUpdateTime" json:"updatedAt"`
+	DeletedAt    *gorm.DeletedAt       `gorm:"index" json:"-"`
+	IsPublic     bool                  `gorm:"default:true" json:"isPublic"`
+	Locations    *[]CollectionLocation `gorm:"foreignKey:CollectionID;constraint:OnDelete:CASCADE" json:"locations"`
+	NumLocations int                   `gorm:"-" json:"numLocations"`
+	NumSaves     int                   `gorm:"-" json:"numSaves"`
+	Contributors []AccountMiniDTO      `gorm:"-" json:"contributors,omitempty"`
+	LocationTags []LocationTag         `gorm:"many2many:collection_location_tags;constraint:OnDelete:CASCADE" json:"locationTags"`
+	PlacesByID   pq.Int32Array         `gorm:"type:integer[]" json:"placesById"`
+	Featured     bool                  `gorm:"default:false" json:"featured"`
 }
 
 func (Collection) TableName() string {
