61424f4466cdd1916eb56558d160a87a79c1d769
noib <b_n_79@protonmail.com>
2025-09-09 01:41:26 +0800

    Add on delete cascade for CalendarEventNote

--- calendar_event_note.go
+++ calendar_event_note.go
@@ -4,5 +4,5 @@ type CalendarEventNote struct {
 	ID              uint `gorm:"primaryKey" json:"id"`
 	CalendarEventID uint `json:"calendarEventId"`
 	NoteID          uint `json:"noteId"`
-	Note            Note `json:"note"`
+	Note            Note `gorm:"constraint:OnDelete:CASCADE;" json:"note"`
 }
