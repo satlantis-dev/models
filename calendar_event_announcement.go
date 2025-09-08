631f74718afd3cf955509b4c7c8c280315bd2bc6
noib <b_n_79@protonmail.com>
2025-09-09 01:51:35 +0800

    Add cascade on delete for CalendarEventAnnouncement

--- calendar_event_announcement.go
+++ calendar_event_announcement.go
@@ -1,8 +1,9 @@
 package models
 
 type CalendarEventAnnouncement struct {
-	ID              uint `gorm:"primaryKey" json:"id"`
-	CalendarEventID uint `json:"calendarEventId"`
-	NoteID          uint `json:"noteId"`
-	Note            Note `json:"note"`
+	ID              uint           `gorm:"primaryKey" json:"id"`
+	CalendarEventID uint           `json:"calendarEventId"`
+	CalendarEvent   *CalendarEvent `gorm:"constraint:OnDelete:CASCADE;" json:"calendarEvent,omitempty"`
+	NoteID          uint           `json:"noteId"`
+	Note            *Note          `gorm:"constraint:OnDelete:CASCADE;" json:"note,omitempty"`
 }
