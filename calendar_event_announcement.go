33693b093ee1a1c5ef78eeaa8b1340c41714385f
noib <b_n_79@protonmail.com>
2025-09-09 02:09:15 +0800

    Tidy up

--- calendar_event_announcement.go
+++ calendar_event_announcement.go
@@ -1,9 +1,8 @@
 package models
 
 type CalendarEventAnnouncement struct {
-	ID              uint           `gorm:"primaryKey" json:"id"`
-	CalendarEventID uint           `json:"calendarEventId"`
-	CalendarEvent   *CalendarEvent `gorm:"constraint:OnDelete:CASCADE;" json:"calendarEvent,omitempty"`
-	NoteID          uint           `json:"noteId"`
-	Note            *Note          `gorm:"constraint:OnDelete:CASCADE;" json:"note,omitempty"`
+	ID              uint  `gorm:"primaryKey" json:"id"`
+	CalendarEventID uint  `json:"calendarEventId"`
+	NoteID          uint  `json:"noteId"`
+	Note            *Note `gorm:"constraint:OnDelete:CASCADE;" json:"note,omitempty"`
 }
