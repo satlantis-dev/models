0609a8962a3ad78121c9749e3a70de37759f3677
noib <b_n_79@protonmail.com>
2025-09-13 01:53:37 +0800

    Add missing notification type

--- notification.go
+++ notification.go
@@ -14,6 +14,7 @@ const (
 	NotificationTypeCalendarEventDiscussionMention        = "calendar_event_discussion_mention"
 	NotificationTypeCalendarEventRSVP                     = "calendar_event_rsvp"
 	NotificationTypeCalendarEventUpdate                   = "calendar_event_update"
+	NotificationTypeCalendarEventRsvpGatedRejected        = "calendar_event_rsvp_gated_rejected"
 	NotificationTypeCollectionImportSuccessful            = "collection_import_successful"
 	NotificationTypeCollectionInvitation                  = "collection_invitation"
 	NotificationTypeCollectionInvitationAccepted          = "collection_invitation_accepted"
