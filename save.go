58b26256a862181c6f19c93156507a471089f557
noib <b_n_79@protonmail.com>
2025-09-23 23:35:39 +0800

    Add foreign key constraints

--- save.go
+++ save.go
@@ -3,7 +3,9 @@ package models
 import "time"
 
 type Save struct {
-	AccountID    uint      `gorm:"index:idx_save,unique"`
-	CollectionID uint      `gorm:"index:idx_save,unique"`
-	CreatedAt    time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"-"`
+	AccountID    uint        `gorm:"index;uniqueIndex:idx_save_unique" json:"accountId"`
+	Account      *Account    `gorm:"foreignKey:AccountID;constraint:OnDelete:CASCADE;" json:"account,omitempty"`
+	CollectionID uint        `gorm:"index;uniqueIndex:idx_save_unique" json:"collectionId"`
+	Collection   *Collection `gorm:"foreignKey:CollectionID;constraint:OnDelete:CASCADE;" json:"collection,omitempty"`
+	CreatedAt    time.Time   `gorm:"default:CURRENT_TIMESTAMP" json:"-"`
 }
