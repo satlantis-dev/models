e8ff80a769f67055d01611341cb055c5676275f6
noib <b_n_79@protonmail.com>
2025-10-24 11:49:15 +0800

    Omit empty events

--- calendar.go
+++ calendar.go
@@ -7,17 +7,17 @@ import (
 )
 
 type Calendar struct {
-	ID          uint            `json:"id"`
-	Name        string          `json:"name"`
-	Description string          `json:"description"`
-	Slug        string          `gorm:"size:70" json:"slug"`
-	Banner      string          `json:"banner"`
-	AccountID   uint            `json:"account_id"`
-	Account     *Account        `gorm:"foreignKey:AccountID;constraint:OnDelete:CASCADE" json:"account,omitempty"`
-	Events      []CalendarEvent `gorm:"many2many:calendar_calendar_events;" json:"events"`
-	EventCount  *int64          `json:"eventCount,omitempty"`
-	IsPublic    bool            `gorm:"default:true" json:"isPublic"`
-	CreatedAt   time.Time       `gorm:"autoCreateTime" json:"createdAt"`
-	UpdatedAt   time.Time       `gorm:"autoUpdateTime" json:"updatedAt"`
-	DeletedAt   *gorm.DeletedAt `gorm:"index" json:"-"`
+	ID          uint             `json:"id"`
+	Name        string           `json:"name"`
+	Description string           `json:"description"`
+	Slug        string           `gorm:"size:70" json:"slug"`
+	Banner      string           `json:"banner"`
+	AccountID   uint             `json:"account_id"`
+	Account     *Account         `gorm:"foreignKey:AccountID;constraint:OnDelete:CASCADE" json:"account,omitempty"`
+	Events      *[]CalendarEvent `gorm:"many2many:calendar_calendar_events;" json:"events,omitempty"`
+	EventCount  *int64           `json:"eventCount,omitempty"`
+	IsPublic    bool             `gorm:"default:true" json:"isPublic"`
+	CreatedAt   time.Time        `gorm:"autoCreateTime" json:"createdAt"`
+	UpdatedAt   time.Time        `gorm:"autoUpdateTime" json:"updatedAt"`
+	DeletedAt   *gorm.DeletedAt  `gorm:"index" json:"-"`
 }
