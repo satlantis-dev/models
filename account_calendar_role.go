da3a475ebd3f1ed10f6f85ac944bb8bbc4bdcdd5
noib <b_n_79@protonmail.com>
2025-10-25 13:30:14 +0800

    Add AccountCalendarRole

---
+++ account_calendar_role.go
@@ -0,0 +1,23 @@
+type AccountCalendarRoleType int
+
+const (
+	CalendarOwner AccountCalendarRoleType = iota + 1
+	CalendarContributor
+	CalendarViewer
+	CalendarInvited
+)
+
+type AccountCalendarRole struct {
+	AccountID  uint                    `gorm:"not null;index;uniqueIndex:idx_account_calendar_role" json:"accountId"`
+	Account    *Account                `gorm:"foreignKey:AccountID;constraint:OnDelete:CASCADE;" json:"account,omitempty"`
+	CalendarID uint                    `gorm:"not null;index;uniqueIndex:idx_account_calendar_role" json:"calendarId"`
+	Calendar   *Calendar               `gorm:"foreignKey:CalendarID;constraint:OnDelete:CASCADE;" json:"calendar,omitempty"`
+	Type       AccountCalendarRoleType `gorm:"not null" json:"type"`
+	CreatedAt  time.Time               `json:"-"`
+	UpdatedAt  time.Time               `json:"-"`
+	DeletedAt  gorm.DeletedAt          `gorm:"index" json:"-"`
+}
+
+func (AccountCalendarRole) TableName() string {
+	return "account_calendar_roles"
+}
