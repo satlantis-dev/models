6299ff6cd3ce6f158dc8cd0d1005b5acd23739db
noib <b_n_79@protonmail.com>
2025-10-29 02:14:11 +0800

    Cascade on delete for Calendar.Events

--- calendar.go
+++ calendar.go
@@ -14,7 +14,7 @@ type Calendar struct {
 	Banner      string           `json:"banner"`
 	AccountID   uint             `json:"account_id"`
 	Account     *Account         `gorm:"foreignKey:AccountID;constraint:OnDelete:CASCADE" json:"account,omitempty"`
-	Events      *[]CalendarEvent `gorm:"many2many:calendar_calendar_events;" json:"events,omitempty"`
+	Events      *[]CalendarEvent `gorm:"many2many:calendar_calendar_events;constraint:OnDelete:CASCADE;" json:"events,omitempty"`
 	EventCount  *int64           `json:"eventCount,omitempty"`
 	IsPublic    bool             `gorm:"default:true" json:"isPublic"`
 	CreatedAt   time.Time        `gorm:"autoCreateTime" json:"createdAt"`
