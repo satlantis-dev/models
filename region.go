502efb75e95462abedf491139953bb263e4c338d
noib <b_n_79@protonmail.com>
2025-10-30 10:43:16 +0800

    Remove region metrics

--- region.go
+++ region.go
@@ -5,19 +5,17 @@ import (
 )
 
 type Region struct {
-	ID             uint                   `gorm:"primaryKey" json:"id"`
-	Code           string                 `gorm:"type:text" json:"code"`
-	CreatedAt      time.Time              `json:"-"`
-	UpdatedAt      time.Time              `json:"-"`
-	DeletedAt      *time.Time             `gorm:"index" json:"-,omitempty"`
-	CategoryScores *[]RegionCategoryScore `gorm:"foreignKey:RegionID" json:"categoryScores,omitempty"`
-	CountryID      uint                   `gorm:"index" json:"countryId"`
-	Country        *Country               `json:"country,omitempty"`
-	Metrics        *[]RegionMetric        `gorm:"foreignKey:RegionID" json:"metrics,omitempty"`
-	Name           string                 `gorm:"index;type:text" json:"name"`
-	OSMID          *uint                  `json:"osmId"`
-	OSMLevel       string                 `json:"osmLevel"`
-	OSMType        OSMType                `json:"osmType"`
-	OSMRef         string                 `gorm:"uniqueIndex" json:"osmRef"`
-	Places         *[]Place               `gorm:"foreignKey:RegionID" json:"places,omitempty"`
+	ID        uint       `gorm:"primaryKey" json:"id"`
+	Code      string     `gorm:"type:text" json:"code"`
+	CreatedAt time.Time  `json:"-"`
+	UpdatedAt time.Time  `json:"-"`
+	DeletedAt *time.Time `gorm:"index" json:"-,omitempty"`
+	CountryID uint       `gorm:"index" json:"countryId"`
+	Country   *Country   `json:"country,omitempty"`
+	Name      string     `gorm:"index;type:text" json:"name"`
+	OSMID     *uint      `json:"osmId"`
+	OSMLevel  string     `json:"osmLevel"`
+	OSMType   OSMType    `json:"osmType"`
+	OSMRef    string     `gorm:"uniqueIndex" json:"osmRef"`
+	Places    *[]Place   `gorm:"foreignKey:RegionID" json:"places,omitempty"`
 }
