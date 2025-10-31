ac416bfab0c4f095f44f9ee4c4a781d147fda86a
noib <b_n_79@protonmail.com>
2025-10-31 17:44:32 +0800

    Fix

--- place_topic_score.go
+++ place_topic_score.go
@@ -6,11 +6,11 @@ import (
 
 type PlaceTopicScore struct {
 	CategoryID uint      `gorm:"primaryKey;index;autoIncrement:false" json:"categoryId"`
-	PlaceID    uint      `gorm:"primaryKey;autoIncrement:false;constraint:OnDelete:CASCADE" json:"placeId"`
+	PlaceID    uint      `gorm:"primaryKey;autoIncrement:false" json:"placeId"`
+	Place      *Place    `gorm:"constraint:OnDelete:CASCADE" json:"-"`
 	Score      float64   `json:"score"`
 	UpdatedAt  time.Time `json:"updatedAt"`
 	TopicID    uint      `gorm:"primaryKey;autoIncrement:false" json:"topicId"`
-	Topic      Topic     `json:"topic"`
 	UserNumber uint      `json:"userNumber"`
 	UserScore  float64   `json:"userScore"`
 }
