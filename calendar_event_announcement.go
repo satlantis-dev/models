85e9a5993eefe0d11487b50c09a26ed7bd51fefc
noib <b_n_79@protonmail.com>
2025-11-17 15:04:16 +0800

    Add email fields

--- calendar_event_announcement.go
+++ calendar_event_announcement.go
@@ -1,12 +1,15 @@
 package models
 
+import "github.com/lib/pq"
+
 type CalendarEventAnnouncement struct {
-	ID              uint    `gorm:"primaryKey" json:"id"`
-	CalendarEventID uint    `json:"calendarEventId"`
-	NoteID          uint    `json:"noteId"`
-	Note            *Note   `gorm:"constraint:OnDelete:CASCADE;" json:"note,omitempty"`
-	ToDiscussion    bool    `gorm:"default:true" json:"toDiscussion"`
-	ToNostr         bool    `gorm:"default:false" json:"toNostr"`
-	ToEmail         bool    `gorm:"default:false" json:"toEmail"`
-	EmailSubject    *string `gorm:"type:text" json:"emailSubject,omitempty"`
+	ID                uint          `gorm:"primaryKey" json:"id"`
+	CalendarEventID   uint          `json:"calendarEventId"`
+	NoteID            uint          `json:"noteId"`
+	Note              *Note         `gorm:"constraint:OnDelete:CASCADE;" json:"note,omitempty"`
+	ToDiscussion      bool          `gorm:"default:true" json:"toDiscussion"`
+	ToNostr           bool          `gorm:"default:false" json:"toNostr"`
+	ToEmail           bool          `gorm:"default:false" json:"toEmail"`
+	EmailSubject      *string       `gorm:"type:text" json:"emailSubject,omitempty"`
+	EmailRecipientIDs pq.Int32Array `gorm:"type:integer[]" json:"emailRecipientIds"`
 }
