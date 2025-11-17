efd009ae98b4293cb13ac65673a708a2752c7e5d
noib <b_n_79@protonmail.com>
2025-11-17 15:01:47 +0800

    Add EmailSubject

--- calendar_event_announcement.go
+++ calendar_event_announcement.go
@@ -1,11 +1,12 @@
 package models
 
 type CalendarEventAnnouncement struct {
-	ID              uint  `gorm:"primaryKey" json:"id"`
-	CalendarEventID uint  `json:"calendarEventId"`
-	NoteID          uint  `json:"noteId"`
-	Note            *Note `gorm:"constraint:OnDelete:CASCADE;" json:"note,omitempty"`
-	ToDiscussion    bool  `gorm:"default:true" json:"toDiscussion"`
-	ToNostr         bool  `gorm:"default:false" json:"toNostr"`
-	ToEmail         bool  `gorm:"default:false" json:"toEmail"`
+	ID              uint    `gorm:"primaryKey" json:"id"`
+	CalendarEventID uint    `json:"calendarEventId"`
+	NoteID          uint    `json:"noteId"`
+	Note            *Note   `gorm:"constraint:OnDelete:CASCADE;" json:"note,omitempty"`
+	ToDiscussion    bool    `gorm:"default:true" json:"toDiscussion"`
+	ToNostr         bool    `gorm:"default:false" json:"toNostr"`
+	ToEmail         bool    `gorm:"default:false" json:"toEmail"`
+	EmailSubject    *string `gorm:"type:text" json:"emailSubject,omitempty"`
 }
