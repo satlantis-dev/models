3a8c146ca39713fc70588cbf55ad201e31c330a1
noib <b_n_79@protonmail.com>
2025-11-18 16:40:36 +0800

    Add calendar event tags

--- interest.go
+++ interest.go
@@ -17,26 +17,27 @@ const (
 )
 
 type Interest struct {
-	ID                    uint             `gorm:"primaryKey" json:"id"`
-	Name                  string           `gorm:"unique" json:"name"`
-	CreatedAt             time.Time        `json:"-"`
-	UpdatedAt             time.Time        `json:"-"`
-	DeletedAt             *gorm.DeletedAt  `gorm:"index" json:"-,omitempty"`
-	Description           string           `gorm:"type:text" json:"description"`
-	RecommendationsByNpub pq.StringArray   `gorm:"type:varchar[]" json:"recommendationsByNpub"`
-	RecommendationsById   pq.Int32Array    `gorm:"type:integer[]" json:"recommendationsById"`
-	AutofollowsByNpub     pq.StringArray   `gorm:"type:varchar[]" json:"autofollowsByNpub"`
-	AutofollowsById       pq.Int32Array    `gorm:"type:integer[]" json:"autofollowsById"`
-	Hashtags              pq.StringArray   `gorm:"type:varchar[]" json:"hashtags"`
-	LocationTags          []LocationTag    `gorm:"many2many:interest_location_tags;constraint:OnDelete:CASCADE;" json:"locationTags"`
-	Category              InterestCategory `json:"category"`
-	Section               string           `json:"section"`
-	Emoji                 string           `gorm:"type:varchar(7)" json:"emoji"`
-	ContentUse            bool             `json:"contentUse"`
-	LocationUse           bool             `json:"locationUse"`
-	EventUse              bool             `json:"eventUse"`
-	PeopleUse             bool             `json:"peopleUse"`
-	IsPrimary             bool             `json:"isPrimary"`
+	ID                    uint               `gorm:"primaryKey" json:"id"`
+	Name                  string             `gorm:"unique" json:"name"`
+	CreatedAt             time.Time          `json:"-"`
+	UpdatedAt             time.Time          `json:"-"`
+	DeletedAt             *gorm.DeletedAt    `gorm:"index" json:"-,omitempty"`
+	Description           string             `gorm:"type:text" json:"description"`
+	RecommendationsByNpub pq.StringArray     `gorm:"type:varchar[]" json:"recommendationsByNpub"`
+	RecommendationsById   pq.Int32Array      `gorm:"type:integer[]" json:"recommendationsById"`
+	AutofollowsByNpub     pq.StringArray     `gorm:"type:varchar[]" json:"autofollowsByNpub"`
+	AutofollowsById       pq.Int32Array      `gorm:"type:integer[]" json:"autofollowsById"`
+	Hashtags              pq.StringArray     `gorm:"type:varchar[]" json:"hashtags"`
+	LocationTags          []LocationTag      `gorm:"many2many:interest_location_tags;constraint:OnDelete:CASCADE;" json:"locationTags"`
+	CalendarEventTags     []CalendarEventTag `gorm:"many2many:interest_calendar_event_tags;constraint:OnDelete:CASCADE;" json:"calendarEventTags"`
+	Category              InterestCategory   `json:"category"`
+	Section               string             `json:"section"`
+	Emoji                 string             `gorm:"type:varchar(7)" json:"emoji"`
+	ContentUse            bool               `json:"contentUse"`
+	LocationUse           bool               `json:"locationUse"`
+	EventUse              bool               `json:"eventUse"`
+	PeopleUse             bool               `json:"peopleUse"`
+	IsPrimary             bool               `json:"isPrimary"`
 }
 
 type WeightedInterests map[uint]float64
