7b0026e400f8ec9f9466343a7421d74006bd1a77
noib <b_n_79@protonmail.com>
2025-11-20 01:51:14 +0800

    Default count to 0

--- calendar_event_tag.go
+++ calendar_event_tag.go
@@ -3,7 +3,7 @@ package models
 type CalendarEventTag struct {
 	ID        uint   `gorm:"primaryKey" json:"id"`
 	Name      string `gorm:"type:text;not null;uniqueIndex" json:"name"`
-	NumEvents int    `gorm:"" json:"numEvents"`
+	NumEvents int    `gorm:"default:0" json:"numEvents"`
 }
 
 func (CalendarEventTag) TableName() string {
