3e7254bf6f3c2349cb6202d9b7ce9b81ea31acad
noib <b_n_79@protonmail.com>
2025-11-26 15:48:24 +0800

    Make Name a unique index

--- persona.go
+++ persona.go
@@ -4,7 +4,7 @@ import "github.com/lib/pq"
 
 type Persona struct {
 	ID          uint          `gorm:"primaryKey" json:"id"`
-	Name        string        `gorm:"type:text" json:"name"`
+	Name        string        `gorm:"type:text;uniqueIndex;not null" json:"name"`
 	Tribe       string        `gorm:"type:text" json:"tribe"`
 	Emoji       string        `gorm:"type:varchar(7)" json:"emoji"`
 	InterestIDs pq.Int32Array `gorm:"type:integer[]" json:"interestIds"`
