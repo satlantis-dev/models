cb2520623a0bc56edb4efaf3704be1dc485a73a5
noib <b_n_79@protonmail.com>
2025-12-04 17:39:33 +0800

    Add date fields

--- calendar_event_tag.go
+++ calendar_event_tag.go
@@ -1,9 +1,18 @@
 package models
 
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
 type CalendarEventTag struct {
-	ID        uint   `gorm:"primaryKey" json:"id,omitempty"`
-	Name      string `gorm:"type:text;not null;uniqueIndex" json:"name,omitempty"`
-	NumEvents *int   `gorm:"default:0" json:"numEvents,omitempty"`
+	ID        uint            `gorm:"primaryKey" json:"id,omitempty"`
+	Name      string          `gorm:"type:text;not null;uniqueIndex" json:"name,omitempty"`
+	NumEvents *int            `gorm:"default:0" json:"numEvents,omitempty"`
+	CreatedAt time.Time       `json:"-"`
+	UpdatedAt time.Time       `json:"-"`
+	DeletedAt *gorm.DeletedAt `gorm:"index" json:"-"`
 }
 
 func (CalendarEventTag) TableName() string {
