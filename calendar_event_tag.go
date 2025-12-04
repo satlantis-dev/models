bf57a176b9ad02ecc43fdeb3f1e99f1d65030706
noib <b_n_79@protonmail.com>
2025-12-04 17:04:34 +0800

    Omit empty

--- calendar_event_tag.go
+++ calendar_event_tag.go
@@ -1,9 +1,9 @@
 package models
 
 type CalendarEventTag struct {
-	ID        uint   `gorm:"primaryKey" json:"id"`
-	Name      string `gorm:"type:text;not null;uniqueIndex" json:"name"`
-	NumEvents *int   `gorm:"default:0" json:"numEvents"`
+	ID        uint   `gorm:"primaryKey" json:"id,omitempty"`
+	Name      string `gorm:"type:text;not null;uniqueIndex" json:"name,omitempty"`
+	NumEvents *int   `gorm:"default:0" json:"numEvents,omitempty"`
 }
 
 func (CalendarEventTag) TableName() string {
