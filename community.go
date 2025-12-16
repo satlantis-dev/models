65dac087e37088712d6695781d8180499c5f9d4a
noib <b_n_79@protonmail.com>
2025-12-16 17:13:57 +0800

    Tidy up

--- community.go
+++ community.go
@@ -8,8 +8,8 @@ import (
 
 type Community struct {
 	ID          uint                   `gorm:"primaryKey;autoIncrement" json:"id"`
-	CreatedAt   time.Time              `gorm:"autoCreateTime" json:"createdAt"`
-	UpdatedAt   time.Time              `gorm:"autoUpdateTime" json:"updatedAt"`
+	CreatedAt   time.Time              `gorm:"autoCreateTime" json:"-"`
+	UpdatedAt   time.Time              `gorm:"autoUpdateTime" json:"-"`
 	DeletedAt   *gorm.DeletedAt        `gorm:"index" json:"-"`
 	Newsletters *[]CommunityNewsletter `gorm:"foreignKey:CommunityID;constraint:OnDelete:CASCADE;" json:"newsletters,omitempty"`
 	Members     *[]CommunityMember     `gorm:"foreignKey:CommunityID;constraint:OnDelete:CASCADE;" json:"members,omitempty"`
