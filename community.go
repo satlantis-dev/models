086ba6725e18f1eddc88a1c4518a0f709b50f356
noib <b_n_79@protonmail.com>
2025-12-17 08:01:50 +0800

    Add community attributes

--- community.go
+++ community.go
@@ -11,6 +11,9 @@ type Community struct {
 	CreatedAt   time.Time              `gorm:"autoCreateTime" json:"-"`
 	UpdatedAt   time.Time              `gorm:"autoUpdateTime" json:"-"`
 	DeletedAt   *gorm.DeletedAt        `gorm:"index" json:"-"`
+	Name        string                 `gorm:"type:text;not null" json:"name"`
+	Description *string                `gorm:"type:text" json:"description,omitempty"`
+	Banner      *string                `gorm:"type:text" json:"banner,omitempty"`
 	Newsletters *[]CommunityNewsletter `gorm:"foreignKey:CommunityID;constraint:OnDelete:CASCADE;" json:"newsletters,omitempty"`
 	Members     *[]CommunityMember     `gorm:"foreignKey:CommunityID;constraint:OnDelete:CASCADE;" json:"members,omitempty"`
 	Calendar    *Calendar              `json:"calendar,omitempty"`
