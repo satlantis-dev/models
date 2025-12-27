8d366eafe31154ce2e1ddfb277cdf36b87cae560
noib <b_n_79@protonmail.com>
2025-12-27 20:21:00 +0800

    Update field name

--- onboarding_profile.go
+++ onboarding_profile.go
@@ -7,15 +7,15 @@ import (
 )
 
 type OnboardingProfile struct {
-	AccountID                  uint            `gorm:"primaryKey" json:"accountId"`
-	Account                    *AccountDTO     `gorm:"foreignKey:AccountID;references:ID;constraint:OnDelete:CASCADE" json:"account,omitempty"`
-	CreatedAt                  time.Time       `gorm:"autoCreateTime" json:"createdAt"`
-	UpdatedAt                  time.Time       `gorm:"autoUpdateTime" json:"updatedAt"`
-	IsHost                     *bool           `json:"isHost,omitempty"`
-	SuggestedLocationGoogleIDs *pq.StringArray `gorm:"type:text[]" json:"suggestedLocationGoogleIds,omitempty"`
-	InterestStatement          *string         `gorm:"type:text" json:"interestStatement,omitempty"`
-	InferredInterests          *pq.StringArray `gorm:"type:text[]" json:"inferredInterests,omitempty"`
-	InferredInterestIDs        *pq.Int32Array  `gorm:"type:integer[]" json:"inferredInterestIds,omitempty"`
+	AccountID           uint            `gorm:"primaryKey" json:"accountId"`
+	Account             *AccountDTO     `gorm:"foreignKey:AccountID;references:ID;constraint:OnDelete:CASCADE" json:"account,omitempty"`
+	CreatedAt           time.Time       `gorm:"autoCreateTime" json:"createdAt"`
+	UpdatedAt           time.Time       `gorm:"autoUpdateTime" json:"updatedAt"`
+	IsHost              *bool           `json:"isHost,omitempty"`
+	SuggestedGoogleIDs  *pq.StringArray `gorm:"type:text[]" json:"suggestedGoogleIds,omitempty"`
+	InterestStatement   *string         `gorm:"type:text" json:"interestStatement,omitempty"`
+	InferredInterests   *pq.StringArray `gorm:"type:text[]" json:"inferredInterests,omitempty"`
+	InferredInterestIDs *pq.Int32Array  `gorm:"type:integer[]" json:"inferredInterestIds,omitempty"`
 }
 
 func (OnboardingProfile) TableName() string {
