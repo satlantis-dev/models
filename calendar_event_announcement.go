8af4b5853d9426002cd2bf8305382ca1e06215ae
noib <b_n_79@protonmail.com>
2026-01-20 17:58:22 +0800

    Show deletedAt

--- calendar_event_announcement.go
+++ calendar_event_announcement.go
@@ -14,7 +14,7 @@ type CalendarEventAnnouncement struct {
 	Note              *Note           `gorm:"constraint:OnDelete:CASCADE;" json:"note,omitempty"`
 	CreatedAt         time.Time       `gorm:"autoCreateTime" json:"createdAt"`
 	UpdatedAt         time.Time       `gorm:"autoUpdateTime" json:"updatedAt"`
-	DeletedAt         *gorm.DeletedAt `gorm:"index" json:"-"`
+	DeletedAt         *gorm.DeletedAt `gorm:"index" json:"deletedAt"`
 	ToNostr           bool            `gorm:"default:false" json:"toNostr"`
 	ToDiscussion      bool            `gorm:"default:true" json:"toDiscussion"`
 	ToEmail           bool            `gorm:"default:true" json:"toEmail"`
