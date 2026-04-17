7fda6833150bd9585b683106ccf39f8011c365cb
noib <b_n_79@protonmail.com>
2026-04-17 20:00:37 +0800

    Remove dates from json

--- themes.go
+++ themes.go
@@ -9,8 +9,8 @@ import (
 type Theme struct {
 	ID              uint            `gorm:"primaryKey;autoIncrement" json:"id"`
 	Name            string          `gorm:"not null;uniqueIndex" json:"name"`
-	CreatedAt       time.Time       `gorm:"autoCreateTime" json:"createdAt"`
-	UpdatedAt       time.Time       `gorm:"autoUpdateTime" json:"updatedAt"`
+	CreatedAt       time.Time       `gorm:"autoCreateTime" json:"-"`
+	UpdatedAt       time.Time       `gorm:"autoUpdateTime" json:"-"`
 	DeletedAt       *gorm.DeletedAt `gorm:"index" json:"-"`
 	BackgroundColor string          `gorm:"type:varchar(7);not null" json:"backgroundColor"`
 	ForegroundColor string          `gorm:"type:varchar(7);not null" json:"foregroundColor"`
