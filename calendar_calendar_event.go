7fb928e05d62662f8af1a7c0223e3a2604a8c62a
noib <b_n_79@protonmail.com>
2026-04-21 00:59:33 +0800

    Fix

--- calendar_calendar_event.go
+++ calendar_calendar_event.go
@@ -1,9 +1,9 @@
 package models
 
 type CalendarCalendarEvent struct {
-	CalendarID      uint           `gorm:"index;uniqueIndex:idx_calendar_calendar_event" json:"calendarId"`
+	CalendarID      uint           `gorm:"primaryKey;index;uniqueIndex:idx_calendar_calendar_event" json:"calendarId"`
 	Calendar        *Calendar      `gorm:"foreignKey:CalendarID;constraint:OnDelete:CASCADE" json:"calendar,omitempty"`
-	CalendarEventID uint           `gorm:"index;uniqueIndex:idx_calendar_calendar_event" json:"calendarEventId"`
+	CalendarEventID uint           `gorm:"primaryKey;index;uniqueIndex:idx_calendar_calendar_event" json:"calendarEventId"`
 	CalendarEvent   *CalendarEvent `gorm:"foreignKey:CalendarEventID;constraint:OnDelete:CASCADE" json:"calendarEvent,omitempty"`
 	Featured        bool           `gorm:"default:false" json:"featured"`
 }
