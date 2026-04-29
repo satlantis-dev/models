32f1ef8f105cf00d0b0610642712630b9742f6e1
noib <b_n_79@protonmail.com>
2026-04-29 17:26:23 +0800

    Add open subscriptions

--- community_member.go
+++ community_member.go
@@ -7,21 +7,21 @@ import (
 )
 
 type CommunityMember struct {
-	ID                  uint                             `gorm:"primaryKey;autoIncrement" json:"id"`
-	CommunityID         uint                             `gorm:"not null;index;uniqueIndex:idx_community_account" json:"communityId"`
-	Community           *Community                       `gorm:"foreignKey:CommunityID;constraint:OnDelete:CASCADE;" json:"community,omitempty"`
-	AccountID           uint                             `gorm:"not null;index;uniqueIndex:idx_community_account" json:"accountId"`
-	Account             *AccountDTO                      `gorm:"foreignKey:AccountID;constraint:OnDelete:CASCADE;" json:"account,omitempty"`
-	TierID              *uint                            `gorm:"index" json:"tierId,omitempty"`
-	Tier                *CommunityMembershipTier         `gorm:"foreignKey:TierID;constraint:OnDelete:SET NULL;" json:"tier,omitempty"`
-	Subscription        *CommunityMembershipSubscription `gorm:"-" json:"subscription,omitempty"`
-	RegistrationAnswers *map[string]interface{}          `gorm:"type:jsonb;serializer:json" json:"registrationAnswers,omitempty"`
-	StartDate           *time.Time                       `gorm:"type:timestamptz" json:"startDate,omitempty"`
-	ExpiryDate          *time.Time                       `gorm:"type:timestamptz;index" json:"expiryDate,omitempty"`
-	IsCommunityAdmin    bool                             `gorm:"not null;default:false" json:"isCommunityAdmin"`
-	CreatedAt           time.Time                        `gorm:"autoCreateTime" json:"createdAt"`
-	UpdatedAt           time.Time                        `gorm:"autoUpdateTime" json:"updatedAt"`
-	DeletedAt           *gorm.DeletedAt                  `gorm:"index" json:"-"`
+	ID                  uint                               `gorm:"primaryKey;autoIncrement" json:"id"`
+	CommunityID         uint                               `gorm:"not null;index;uniqueIndex:idx_community_account" json:"communityId"`
+	Community           *Community                         `gorm:"foreignKey:CommunityID;constraint:OnDelete:CASCADE;" json:"community,omitempty"`
+	AccountID           uint                               `gorm:"not null;index;uniqueIndex:idx_community_account" json:"accountId"`
+	Account             *AccountDTO                        `gorm:"foreignKey:AccountID;constraint:OnDelete:CASCADE;" json:"account,omitempty"`
+	TierID              *uint                              `gorm:"index" json:"tierId,omitempty"`
+	Tier                *CommunityMembershipTier           `gorm:"foreignKey:TierID;constraint:OnDelete:SET NULL;" json:"tier,omitempty"`
+	OpenSubscriptions   *[]CommunityMembershipSubscription `gorm:"-" json:"openSubscriptions,omitempty"`
+	RegistrationAnswers *map[string]interface{}            `gorm:"type:jsonb;serializer:json" json:"registrationAnswers,omitempty"`
+	StartDate           *time.Time                         `gorm:"type:timestamptz" json:"startDate,omitempty"`
+	ExpiryDate          *time.Time                         `gorm:"type:timestamptz;index" json:"expiryDate,omitempty"`
+	IsCommunityAdmin    bool                               `gorm:"not null;default:false" json:"isCommunityAdmin"`
+	CreatedAt           time.Time                          `gorm:"autoCreateTime" json:"createdAt"`
+	UpdatedAt           time.Time                          `gorm:"autoUpdateTime" json:"updatedAt"`
+	DeletedAt           *gorm.DeletedAt                    `gorm:"index" json:"-"`
 }
 
 func (CommunityMember) TableName() string {
