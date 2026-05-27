4f7409060fdda02a41af59950709836aa00c3836
noib <b_n_79@protonmail.com>
2026-05-27 18:21:11 +0800

    Remove unique constraint

--- community_gallery_image.go
+++ community_gallery_image.go
@@ -5,7 +5,7 @@ import "time"
 type CommunityGalleryImage struct {
 	ID          uint      `gorm:"primaryKey;autoIncrement" json:"id"`
 	CommunityID uint      `gorm:"not null;index" json:"communityId"`
-	Url         string    `gorm:"not null;unique" json:"url"`
+	Url         string    `gorm:"not null" json:"url"`
 	Rank        int       `gorm:"not null;default:0" json:"rank"`
 	CreatedAt   time.Time `json:"createdAt"`
 }
