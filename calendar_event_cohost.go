1f2f43cff0e0818b692643854befbbb764b83acc
noib <b_n_79@protonmail.com>
2026-06-07 10:58:07 +0800

    Add CalendarEventCohost.Role

--- calendar_event_cohost.go
+++ calendar_event_cohost.go
@@ -4,16 +4,23 @@ import (
 	"time"
 )
 
+type CalendarEventCohostRole string
+
+const (
+	CalendarEventCohostRoleAdmin CalendarEventCohostRole = "admin"
+	CalendarEventCohostRoleStaff CalendarEventCohostRole = "staff"
+)
+
 type CalendarEventCohost struct {
-	ID                   uint           `gorm:"primaryKey" json:"id"`
-	CalendarEventID      uint           `gorm:"index;not null" json:"calendarEventId"`
-	CalendarEvent        *CalendarEvent `gorm:"foreignKey:CalendarEventID" json:"calendarEvent,omitempty"`
-	AccountID            uint           `gorm:"index;not null" json:"accountId"`
-	Account              *AccountDTO    `gorm:"foreignKey:AccountID" json:"account"`
-	InvitationAcceptedAt *time.Time     `json:"invitationAcceptedAt"`
-	InvitationDeclinedAt *time.Time     `json:"invitationDeclinedAt"`
-	AutoAcceptInvitation bool           `gorm:"default:false" json:"-"`
-	IsEmailAdded         bool           `gorm:"default:false" json:"-"`
-	CreatedAt            time.Time      `json:"createdAt"`
-	UpdatedAt            time.Time      `json:"updatedAt"`
+	ID                   uint                    `gorm:"primaryKey" json:"id"`
+	CalendarEventID      uint                    `gorm:"index;not null" json:"calendarEventId"`
+	CalendarEvent        *CalendarEvent          `gorm:"foreignKey:CalendarEventID" json:"calendarEvent,omitempty"`
+	AccountID            uint                    `gorm:"index;not null" json:"accountId"`
+	Account              *AccountDTO             `gorm:"foreignKey:AccountID" json:"account"`
+	Role                 CalendarEventCohostRole `gorm:"default:admin" json:"role"`
+	InvitationAcceptedAt *time.Time              `json:"invitationAcceptedAt"`
+	InvitationDeclinedAt *time.Time              `json:"invitationDeclinedAt"`
+	AutoAcceptInvitation bool                    `gorm:"default:false" json:"-"`
+	CreatedAt            time.Time               `json:"createdAt"`
+	UpdatedAt            time.Time               `json:"updatedAt"`
 }
