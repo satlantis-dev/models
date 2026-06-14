2acb2bca2638b777a3c61181394a48c89d02c468
noib <b_n_79@protonmail.com>
2026-06-14 21:51:10 +0800

    Tidy up

--- account_community_role.go
+++ account_community_role.go
@@ -6,24 +6,24 @@ import (
 	"gorm.io/gorm"
 )
 
-type CommunityRoleType string
+type AccountCommunityRoleType string
 
 const (
-	CommunityRoleAdmin CommunityRoleType = "admin"
+	AccountCommunityRoleAdmin AccountCommunityRoleType = "admin"
 )
 
 type AccountCommunityRole struct {
-	AccountID   uint              `gorm:"not null;index;uniqueIndex:idx_account_community_role" json:"accountId"`
-	Account     *AccountDTO       `gorm:"foreignKey:AccountID;constraint:OnDelete:CASCADE;" json:"account,omitempty"`
-	CommunityID uint              `gorm:"not null;index;uniqueIndex:idx_account_community_role" json:"communityId"`
-	Community   *Community        `gorm:"foreignKey:CommunityID;constraint:OnDelete:CASCADE;" json:"community,omitempty"`
-	Role                 CommunityRoleType `gorm:"type:varchar(32);not null;uniqueIndex:idx_account_community_role" json:"role"`
-	InvitationReceivedAt *time.Time        `gorm:"type:timestamptz" json:"invitationReceivedAt,omitempty"`
-	InvitationAcceptedAt *time.Time        `gorm:"type:timestamptz" json:"invitationAcceptedAt,omitempty"`
-	InvitationDeclinedAt *time.Time        `gorm:"type:timestamptz" json:"invitationDeclinedAt,omitempty"`
-	CreatedAt            time.Time         `json:"createdAt"`
-	UpdatedAt            time.Time         `json:"updatedAt"`
-	DeletedAt            gorm.DeletedAt    `gorm:"index" json:"-"`
+	AccountID            uint                     `gorm:"not null;index;uniqueIndex:idx_account_community_role" json:"accountId"`
+	Account              *AccountDTO              `gorm:"foreignKey:AccountID;constraint:OnDelete:CASCADE;" json:"account,omitempty"`
+	CommunityID          uint                     `gorm:"not null;index;uniqueIndex:idx_account_community_role" json:"communityId"`
+	Community            *Community               `gorm:"foreignKey:CommunityID;constraint:OnDelete:CASCADE;" json:"community,omitempty"`
+	Type                 AccountCommunityRoleType `gorm:"type:varchar(32);not null;uniqueIndex:idx_account_community_role" json:"type"`
+	InvitationReceivedAt *time.Time               `gorm:"type:timestamptz" json:"invitationReceivedAt,omitempty"`
+	InvitationAcceptedAt *time.Time               `gorm:"type:timestamptz" json:"invitationAcceptedAt,omitempty"`
+	InvitationDeclinedAt *time.Time               `gorm:"type:timestamptz" json:"invitationDeclinedAt,omitempty"`
+	CreatedAt            time.Time                `json:"createdAt"`
+	UpdatedAt            time.Time                `json:"updatedAt"`
+	DeletedAt            gorm.DeletedAt           `gorm:"index" json:"-"`
 }
 
 func (AccountCommunityRole) TableName() string {
