c3cd55e4fb9a691181eadccf6f6b52fc706f5b1d
noib <b_n_79@protonmail.com>
2026-06-15 16:26:49 +0800

    Tidy up

--- calendar_event_cohost.go
+++ calendar_event_cohost.go
@@ -13,11 +13,11 @@ const (
 
 type CalendarEventCohost struct {
 	ID                   uint                        `gorm:"primaryKey" json:"id"`
-	CalendarEventID      uint                        `gorm:"index:idx_cohost_event_role,not null" json:"calendarEventId"`
+	CalendarEventID      uint                        `gorm:"not null;index:idx_cohost_event_role" json:"calendarEventId"`
 	CalendarEvent        *CalendarEvent              `gorm:"foreignKey:CalendarEventID" json:"calendarEvent,omitempty"`
-	AccountID            uint                        `gorm:"index;not null" json:"accountId"`
+	AccountID            uint                        `gorm:"not null;index:idx_cohost_event_role" json:"accountId"`
 	Account              *AccountDTO                 `gorm:"foreignKey:AccountID" json:"account"`
-	Type                 CalendarEventCohostRoleType `gorm:"index:idx_cohost_event_role;default:admin" json:"type"`
+	Type                 CalendarEventCohostRoleType `gorm:"type:varchar(32);not null;default:'admin';index:idx_cohost_event_role" json:"type"`
 	InvitationReceivedAt *time.Time                  `gorm:"type:timestamptz" json:"invitationReceivedAt,omitempty"`
 	InvitationAcceptedAt *time.Time                  `gorm:"type:timestamptz" json:"invitationAcceptedAt,omitempty"`
 	InvitationDeclinedAt *time.Time                  `gorm:"type:timestamptz" json:"invitationDeclinedAt,omitempty"`
