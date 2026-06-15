07a679e008819caf9ce5525b842211dff89c3c5d
noib <b_n_79@protonmail.com>
2026-06-15 15:39:32 +0800

    Harmonize

--- calendar_event_cohost.go
+++ calendar_event_cohost.go
@@ -4,24 +4,24 @@ import (
 	"time"
 )
 
-type CalendarEventCohostRole string
+type CalendarEventCohostRoleType string
 
 const (
-	CalendarEventCohostRoleAdmin CalendarEventCohostRole = "admin"
-	CalendarEventCohostRoleStaff CalendarEventCohostRole = "staff"
+	CalendarEventCohostRoleAdmin CalendarEventCohostRoleType = "admin"
+	CalendarEventCohostRoleStaff CalendarEventCohostRoleType = "staff"
 )
 
 type CalendarEventCohost struct {
-	ID                   uint                    `gorm:"primaryKey" json:"id"`
-	CalendarEventID      uint                    `gorm:"index:idx_cohost_event_role,not null" json:"calendarEventId"`
-	CalendarEvent        *CalendarEvent          `gorm:"foreignKey:CalendarEventID" json:"calendarEvent,omitempty"`
-	AccountID            uint                    `gorm:"index;not null" json:"accountId"`
-	Account              *AccountDTO             `gorm:"foreignKey:AccountID" json:"account"`
-	Role                 CalendarEventCohostRole `gorm:"index:idx_cohost_event_role;default:admin" json:"role"`
-	InvitationReceivedAt *time.Time              `gorm:"type:timestamptz" json:"invitationReceivedAt,omitempty"`
-	InvitationAcceptedAt *time.Time              `gorm:"type:timestamptz" json:"invitationAcceptedAt,omitempty"`
-	InvitationDeclinedAt *time.Time              `gorm:"type:timestamptz" json:"invitationDeclinedAt,omitempty"`
-	AutoAcceptInvitation bool                    `gorm:"default:false" json:"-"`
-	CreatedAt            time.Time               `json:"createdAt"`
-	UpdatedAt            time.Time               `json:"updatedAt"`
+	ID                   uint                        `gorm:"primaryKey" json:"id"`
+	CalendarEventID      uint                        `gorm:"index:idx_cohost_event_role,not null" json:"calendarEventId"`
+	CalendarEvent        *CalendarEvent              `gorm:"foreignKey:CalendarEventID" json:"calendarEvent,omitempty"`
+	AccountID            uint                        `gorm:"index;not null" json:"accountId"`
+	Account              *AccountDTO                 `gorm:"foreignKey:AccountID" json:"account"`
+	Type                 CalendarEventCohostRoleType `gorm:"index:idx_cohost_event_role;default:admin" json:"type"`
+	InvitationReceivedAt *time.Time                  `gorm:"type:timestamptz" json:"invitationReceivedAt,omitempty"`
+	InvitationAcceptedAt *time.Time                  `gorm:"type:timestamptz" json:"invitationAcceptedAt,omitempty"`
+	InvitationDeclinedAt *time.Time                  `gorm:"type:timestamptz" json:"invitationDeclinedAt,omitempty"`
+	AutoAcceptInvitation bool                        `gorm:"default:false" json:"-"`
+	CreatedAt            time.Time                   `json:"createdAt"`
+	UpdatedAt            time.Time                   `json:"updatedAt"`
 }
