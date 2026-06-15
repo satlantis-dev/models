9d239aa89a0d2da56034395441152cc69d9de803
noib <b_n_79@protonmail.com>
2026-06-15 16:48:02 +0800

    Fix unique index

--- calendar_event_cohost.go
+++ calendar_event_cohost.go
@@ -13,11 +13,11 @@ const (
 
 type CalendarEventCohost struct {
 	ID                   uint                        `gorm:"primaryKey" json:"id"`
-	CalendarEventID      uint                        `gorm:"not null;index;index:idx_cohost_event_role" json:"calendarEventId"`
+	CalendarEventID      uint                        `gorm:"not null;index;uniqueIndex:idx_cohost_event_account" json:"calendarEventId"`
 	CalendarEvent        *CalendarEvent              `gorm:"foreignKey:CalendarEventID" json:"calendarEvent,omitempty"`
-	AccountID            uint                        `gorm:"not null;index;index:idx_cohost_event_role" json:"accountId"`
+	AccountID            uint                        `gorm:"not null;index;uniqueIndex:idx_cohost_event_account" json:"accountId"`
 	Account              *AccountDTO                 `gorm:"foreignKey:AccountID" json:"account"`
-	Type                 CalendarEventCohostRoleType `gorm:"type:varchar(32);not null;default:'admin';index:idx_cohost_event_role" json:"type"`
+	Type                 CalendarEventCohostRoleType `gorm:"type:varchar(32);not null;default:'admin';index" json:"type"`
 	InvitationReceivedAt *time.Time                  `gorm:"type:timestamptz" json:"invitationReceivedAt,omitempty"`
 	InvitationAcceptedAt *time.Time                  `gorm:"type:timestamptz" json:"invitationAcceptedAt,omitempty"`
 	InvitationDeclinedAt *time.Time                  `gorm:"type:timestamptz" json:"invitationDeclinedAt,omitempty"`
