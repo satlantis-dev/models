fcf12b2c8e1420d0ec77e2b9af138ab5d06ed1c0
noib <b_n_79@protonmail.com>
2026-06-21 20:09:25 +0800

    Add community wallet

--- database.go
+++ database.go
@@ -56,6 +56,9 @@ var DatabaseModels = []interface{}{
 	CommunityMembershipSubscriptionChange{},
 	CommunityMembershipPayment{},
 	CommunityMembershipRefund{},
+	CommunityWallet{},
+	CommunityWalletWithdrawal{},
+	CommunityWalletTransaction{},
 	CommunityNewsletter{},
 	AccountCommunityRole{},
 	// Collection related:
