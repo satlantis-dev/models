1b1ac63413c100ebdc337d6c20a4b12e8d2aa00e
noib <b_n_79@protonmail.com>
2026-08-24 23:06:55 +0800

    Add owner role

--- account_community_role.go
+++ account_community_role.go
@@ -9,6 +9,7 @@ import (
 type AccountCommunityRoleType string
 
 const (
+	AccountCommunityRoleOwner AccountCommunityRoleType = "admin"
 	AccountCommunityRoleAdmin AccountCommunityRoleType = "admin"
 )
 
