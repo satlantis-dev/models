75361f5513d0024ceceb2286858559a6418d4792
noib <b_n_79@protonmail.com>
2026-09-15 17:01:30 +0800

    Add BTC discount

--- plan.go
+++ plan.go
@@ -7,19 +7,20 @@ import (
 )
 
 type Plan struct {
-	ID            uint            `gorm:"primaryKey;autoIncrement" json:"id"`
-	Name          string          `gorm:"type:text;not null;uniqueIndex" json:"name"`
-	Description   *string         `gorm:"type:text" json:"description,omitempty"`
-	Currency      OrderCurrency   `gorm:"type:varchar(8);not null" json:"currency"`
-	MonthlyAmount *int64          `gorm:"type:bigint" json:"monthlyAmount,omitempty"`
-	YearlyAmount  *int64          `gorm:"type:bigint" json:"yearlyAmount,omitempty"`
-	Rank          int             `gorm:"not null;default:0" json:"rank"`
-	IsPaid        bool            `gorm:"not null;default:false" json:"isPaid"`
-	IsHidden      bool            `gorm:"not null;default:false" json:"isHidden"`
-	TrialDays     int             `gorm:"not null;default:0" json:"trialDays"`
-	CreatedAt     time.Time       `gorm:"autoCreateTime" json:"createdAt"`
-	UpdatedAt     time.Time       `gorm:"autoUpdateTime" json:"updatedAt"`
-	DeletedAt     *gorm.DeletedAt `gorm:"index" json:"-"`
+	ID                    uint            `gorm:"primaryKey;autoIncrement" json:"id"`
+	Name                  string          `gorm:"type:text;not null;uniqueIndex" json:"name"`
+	Description           *string         `gorm:"type:text" json:"description,omitempty"`
+	Currency              OrderCurrency   `gorm:"type:varchar(8);not null" json:"currency"`
+	MonthlyAmount         *int64          `gorm:"type:bigint" json:"monthlyAmount,omitempty"`
+	YearlyAmount          *int64          `gorm:"type:bigint" json:"yearlyAmount,omitempty"`
+	BTCDiscountPercentage *uint           `json:"btcDiscountPercentage,omitempty"`
+	Rank                  int             `gorm:"not null;default:0" json:"rank"`
+	IsPaid                bool            `gorm:"not null;default:false" json:"isPaid"`
+	IsHidden              bool            `gorm:"not null;default:false" json:"isHidden"`
+	TrialDays             int             `gorm:"not null;default:0" json:"trialDays"`
+	CreatedAt             time.Time       `gorm:"autoCreateTime" json:"createdAt"`
+	UpdatedAt             time.Time       `gorm:"autoUpdateTime" json:"updatedAt"`
+	DeletedAt             *gorm.DeletedAt `gorm:"index" json:"-"`
 }
 
 func (Plan) TableName() string {
